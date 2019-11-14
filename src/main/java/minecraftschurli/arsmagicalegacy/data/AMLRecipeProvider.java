dd3f56e8e5c69c5276cebff0ee9d60155c6837c7
Minecraftschurli <minecraftschurli@gmail.com>
2019-11-14 06:53:31 +0100

    fix recipes

--- src/main/java/minecraftschurli/arsmagicalegacy/data/AMLRecipeProvider.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/data/AMLRecipeProvider.java
@@ -2,8 +2,11 @@ package minecraftschurli.arsmagicalegacy.data;
 
 import minecraftschurli.arsmagicalegacy.init.ModItems;
 import minecraftschurli.arsmagicalegacy.init.ModTags;
+import net.minecraft.advancements.criterion.InventoryChangeTrigger;
+import net.minecraft.advancements.criterion.ItemPredicate;
 import net.minecraft.data.*;
 import net.minecraft.item.Items;
+import net.minecraftforge.common.Tags;
 
 import java.util.function.Consumer;
 
@@ -19,26 +22,72 @@ public class AMLRecipeProvider extends RecipeProvider {
     @Override
     protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
         //TODO @IchHabeHunger54 : Register recipes
-        RecipeHelper.addLargeCompressRecipe(ModItems.CHIMERITE_BLOCK.get(), ModItems.CHIMERITE.get());
-        RecipeHelper.addLargeCompressRecipe(ModItems.TOPAZ_BLOCK.get(), ModItems.TOPAZ.get());
-        RecipeHelper.addLargeCompressRecipe(ModItems.VINTEUM_BLOCK.get(), ModItems.VINTEUM.get());
-        RecipeHelper.addLargeCompressRecipe(ModItems.MOONSTONE_BLOCK.get(), ModItems.MOONSTONE.get());
-        RecipeHelper.addLargeCompressRecipe(ModItems.SUNSTONE_BLOCK.get(), ModItems.SUNSTONE.get());
-        RecipeHelper.addLargeDecompressRecipe(ModItems.CHIMERITE.get(), ModItems.CHIMERITE_BLOCK.get());
-        RecipeHelper.addLargeDecompressRecipe(ModItems.CHIMERITE.get(), ModItems.TOPAZ_BLOCK.get());
-        RecipeHelper.addLargeDecompressRecipe(ModItems.CHIMERITE.get(), ModItems.VINTEUM_BLOCK.get());
-        RecipeHelper.addLargeDecompressRecipe(ModItems.CHIMERITE.get(), ModItems.CHIMERITE_BLOCK.get());
-        RecipeHelper.addLargeDecompressRecipe(ModItems.CHIMERITE.get(), ModItems.CHIMERITE_BLOCK.get());
-        RecipeHelper.addSlabRecipe(ModItems.WITCHWOOD_SLAB.get(), ModItems.WITCHWOOD_PLANKS.get());
-        RecipeHelper.addStairRecipe(ModItems.WITCHWOOD_STAIRS.get(), ModItems.WITCHWOOD_PLANKS.get());
-        ShapedRecipeBuilder.shapedRecipe(ModItems.WITCHWOOD_DOOR.get(), 3).patternLine("XX").patternLine("XX").patternLine("XX").key('X', ModItems.WITCHWOOD_PLANKS.get());
-        ShapedRecipeBuilder.shapedRecipe(ModItems.WITCHWOOD_TRAPDOOR.get(), 2).patternLine("XXX").patternLine("XXX").key('X', ModItems.WITCHWOOD_PLANKS.get());
-        ShapedRecipeBuilder.shapedRecipe(ModItems.WITCHWOOD_FENCE.get(), 3).patternLine("XSX").patternLine("XSX").key('X', ModItems.WITCHWOOD_PLANKS.get()).key('S', Items.STICK);
-        ShapedRecipeBuilder.shapedRecipe(ModItems.WITCHWOOD_FENCE_GATE.get(), 3).patternLine("SXS").patternLine("SXS").key('X', ModItems.WITCHWOOD_PLANKS.get()).key('S', Items.STICK);
-        ShapedRecipeBuilder.shapedRecipe(ModItems.WITCHWOOD_PRESSURE_PLATE.get()).patternLine("XX").key('X', ModItems.WITCHWOOD_PLANKS.get());
-        ShapelessRecipeBuilder.shapelessRecipe(ModItems.WITCHWOOD_BUTTON.get()).addIngredient(ModItems.WITCHWOOD_PLANKS.get());
-        ShapelessRecipeBuilder.shapelessRecipe(ModItems.WITCHWOOD_PLANKS.get(), 4).addIngredient(ModTags.Items.LOGS_WITCHWOOD);
-        ShapedRecipeBuilder.shapedRecipe(ModItems.ALTAR_CORE.get()).patternLine("V").patternLine("S").key('V', ModItems.VINTEUM.get()).key('S', Items.STONE);
-        ShapedRecipeBuilder.shapedRecipe(ModItems.MAGIC_WALL.get(), 16).patternLine("VSV").key('V', ModItems.VINTEUM.get()).key('S', Items.STONE);
+        RecipeHelper.addLargeCompressRecipe(ModItems.CHIMERITE_BLOCK.get(), ModTags.Items.GEMS_CHIMERITE).build(consumer);
+        RecipeHelper.addLargeCompressRecipe(ModItems.TOPAZ_BLOCK.get(), ModTags.Items.GEMS_TOPAZ).build(consumer);
+        RecipeHelper.addLargeCompressRecipe(ModItems.VINTEUM_BLOCK.get(), ModTags.Items.DUSTS_VINTEUM).build(consumer);
+        RecipeHelper.addLargeCompressRecipe(ModItems.MOONSTONE_BLOCK.get(), ModTags.Items.GEMS_MOONSTONE).build(consumer);
+        RecipeHelper.addLargeCompressRecipe(ModItems.SUNSTONE_BLOCK.get(), ModTags.Items.GEMS_SUNSTONE).build(consumer);
+        RecipeHelper.addLargeDecompressRecipe(ModItems.CHIMERITE.get(), ModTags.Items.STORAGE_BLOCKS_CHIMERITE).build(consumer);
+        RecipeHelper.addLargeDecompressRecipe(ModItems.MOONSTONE.get(), ModTags.Items.STORAGE_BLOCKS_MOONSTONE).build(consumer);
+        RecipeHelper.addLargeDecompressRecipe(ModItems.SUNSTONE.get(), ModTags.Items.STORAGE_BLOCKS_SUNSTONE).build(consumer);
+        RecipeHelper.addLargeDecompressRecipe(ModItems.TOPAZ.get(), ModTags.Items.STORAGE_BLOCKS_TOPAZ).build(consumer);
+        RecipeHelper.addLargeDecompressRecipe(ModItems.VINTEUM.get(), ModTags.Items.STORAGE_BLOCKS_VINTEUM).build(consumer);
+        RecipeHelper.addSlabRecipe(ModItems.WITCHWOOD_SLAB.get(), ModItems.WITCHWOOD_PLANKS.get()).build(consumer);
+        RecipeHelper.addStairRecipe(ModItems.WITCHWOOD_STAIRS.get(), ModItems.WITCHWOOD_PLANKS.get()).build(consumer);
+        ShapedRecipeBuilder
+                .shapedRecipe(ModItems.WITCHWOOD_DOOR.get(), 3)
+                .patternLine("XX")
+                .patternLine("XX")
+                .patternLine("XX")
+                .key('X', ModItems.WITCHWOOD_PLANKS.get())
+                .addCriterion("item", InventoryChangeTrigger.Instance.forItems(ItemPredicate.Builder.create().item(ModItems.WITCHWOOD_PLANKS.get()).build()))
+                .build(consumer);
+        ShapedRecipeBuilder
+                .shapedRecipe(ModItems.WITCHWOOD_TRAPDOOR.get(), 2)
+                .patternLine("XXX")
+                .patternLine("XXX")
+                .key('X', ModItems.WITCHWOOD_PLANKS.get())
+                .addCriterion("item", InventoryChangeTrigger.Instance.forItems(ItemPredicate.Builder.create().item(ModItems.WITCHWOOD_PLANKS.get()).build()))
+                .build(consumer);
+        ShapedRecipeBuilder
+                .shapedRecipe(ModItems.WITCHWOOD_FENCE.get(), 3)
+                .patternLine("XSX")
+                .patternLine("XSX")
+                .key('X', ModItems.WITCHWOOD_PLANKS.get())
+                .key('S', Items.STICK)
+                .addCriterion("item", InventoryChangeTrigger.Instance.forItems(ItemPredicate.Builder.create().item(ModItems.WITCHWOOD_PLANKS.get()).build()))
+                .build(consumer);
+        ShapedRecipeBuilder
+                .shapedRecipe(ModItems.WITCHWOOD_FENCE_GATE.get(), 3)
+                .patternLine("SXS")
+                .patternLine("SXS")
+                .key('X', ModItems.WITCHWOOD_PLANKS.get())
+                .key('S', Items.STICK)
+                .addCriterion("item", InventoryChangeTrigger.Instance.forItems(ItemPredicate.Builder.create().item(ModItems.WITCHWOOD_PLANKS.get()).build()))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(ModItems.WITCHWOOD_PRESSURE_PLATE.get())
+                .patternLine("XX")
+                .key('X', ModItems.WITCHWOOD_PLANKS.get())
+                .addCriterion("item", InventoryChangeTrigger.Instance.forItems(ItemPredicate.Builder.create().item(ModItems.WITCHWOOD_PLANKS.get()).build()))
+                .build(consumer);
+        ShapelessRecipeBuilder
+                .shapelessRecipe(ModItems.WITCHWOOD_BUTTON.get())
+                .addIngredient(ModItems.WITCHWOOD_PLANKS.get())
+                .addCriterion("item", InventoryChangeTrigger.Instance.forItems(ItemPredicate.Builder.create().item(ModItems.WITCHWOOD_PLANKS.get()).build()))
+                .build(consumer);
+        RecipeHelper.addSmallDecompressRecipe(ModItems.WITCHWOOD_PLANKS.get(), ModTags.Items.LOGS_WITCHWOOD).build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(ModItems.ALTAR_CORE.get())
+                .patternLine("V")
+                .patternLine("S")
+                .key('V', ModTags.Items.DUSTS_VINTEUM)
+                .key('S', Tags.Items.STONE)
+                .addCriterion("item", InventoryChangeTrigger.Instance.forItems(ItemPredicate.Builder.create().tag(ModTags.Items.DUSTS_VINTEUM).build()))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(ModItems.MAGIC_WALL.get(), 16)
+                .patternLine("VSV")
+                .key('V', ModTags.Items.DUSTS_VINTEUM)
+                .key('S', Tags.Items.STONE)
+                .addCriterion("item", InventoryChangeTrigger.Instance.forItems(ItemPredicate.Builder.create().tag(ModTags.Items.DUSTS_VINTEUM).build()))
+                .build(consumer);
     }
 }
