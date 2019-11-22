5a328a0f0a627e61c7969ebbef80080b282c591e
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-11-22 08:54:26 +0100

    Add rune spell

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/tileentity/ModContainer.java
@@ -0,0 +1,8 @@
+package minecraftschurli.arsmagicalegacy.objects.tileentity;
+
+import net.minecraft.inventory.container.Container;
+
+public abstract class ModContainer extends Container {
+    protected ModContainer() {
+    }
+}
