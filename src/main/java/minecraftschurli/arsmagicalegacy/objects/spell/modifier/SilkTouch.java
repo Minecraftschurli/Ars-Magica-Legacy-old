7a0ddb62cc22bbdca107be8113de4ee7cca8eeab
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-11-25 11:50:14 +0100

    Add duration modifier

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/modifier/SilkTouch.java
@@ -0,0 +1,41 @@
+package minecraftschurli.arsmagicalegacy.objects.spell.modifier;
+
+import minecraftschurli.arsmagicalegacy.api.spellsystem.ISpellIngredient;
+import minecraftschurli.arsmagicalegacy.api.spellsystem.ItemStackSpellIngredient;
+import minecraftschurli.arsmagicalegacy.api.spellsystem.ItemTagSpellIngredient;
+import minecraftschurli.arsmagicalegacy.api.spellsystem.SpellModifier;
+import minecraftschurli.arsmagicalegacy.init.ModItems;
+import minecraftschurli.arsmagicalegacy.init.ModTags;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
+
+import java.util.EnumSet;
+
+public class SilkTouch extends SpellModifier {
+    @Override
+    public ISpellIngredient[] getRecipeItems() {
+        return new ISpellIngredient[]{
+                new ItemStackSpellIngredient(new ItemStack(Items.BONE)),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.AIR_ESSENCE.get())),
+                new ItemTagSpellIngredient(ModTags.Items.GEMS_CHIMERITE, 1)
+        };
+    }
+
+    @Override
+    public float getModifier(Type type, LivingEntity caster, Entity target, World world, byte[] metadata) {
+        return 1;
+    }
+
+    @Override
+    public float getManaCostMultiplier(ItemStack spellStack, int stage, int quantity) {
+        return 1.25f * quantity;
+    }
+
+    @Override
+    public EnumSet<Type> getAspectsModified() {
+        return EnumSet.of(Type.SILKTOUCH_LEVEL);
+    }
+}
