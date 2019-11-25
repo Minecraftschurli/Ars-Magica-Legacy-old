4e86b104e677884e29fa91cb960b2a1a5f3260d0
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-11-25 13:19:53 +0100

    Add fortune modifier

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/modifier/Fortune.java
@@ -0,0 +1,37 @@
+package minecraftschurli.arsmagicalegacy.objects.spell.modifier;
+
+import minecraftschurli.arsmagicalegacy.api.spellsystem.ISpellIngredient;
+import minecraftschurli.arsmagicalegacy.api.spellsystem.ItemStackSpellIngredient;
+import minecraftschurli.arsmagicalegacy.api.spellsystem.SpellModifier;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
+
+import java.util.EnumSet;
+
+public class Fortune extends SpellModifier {
+    @Override
+    public ISpellIngredient[] getRecipeItems() {
+        return new ISpellIngredient[]{
+                new ItemStackSpellIngredient(new ItemStack(Items.GOLD_INGOT)),
+                new ItemStackSpellIngredient(new ItemStack(Items.GOLD_INGOT))
+        };
+    }
+
+    @Override
+    public float getModifier(Type type, LivingEntity caster, Entity target, World world, byte[] metadata) {
+        return 1;
+    }
+
+    @Override
+    public float getManaCostMultiplier(ItemStack spellStack, int stage, int quantity) {
+        return 1.25f * quantity;
+    }
+
+    @Override
+    public EnumSet<Type> getAspectsModified() {
+        return EnumSet.of(Type.FORTUNE_LEVEL);
+    }
+}
