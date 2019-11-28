72217e842765ffede1d1079b946a826912e25358
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-11-28 13:20:07 +0100

    Add wall shape

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/shape/Wall.java
@@ -0,0 +1,71 @@
+package minecraftschurli.arsmagicalegacy.objects.spell.shape;
+
+import minecraftschurli.arsmagicalegacy.api.spellsystem.*;
+import minecraftschurli.arsmagicalegacy.init.*;
+import minecraftschurli.arsmagicalegacy.objects.item.*;
+import minecraftschurli.arsmagicalegacy.objects.spell.*;
+import net.minecraft.entity.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.util.*;
+import net.minecraft.world.*;
+
+import java.util.*;
+
+public class Wall extends SpellShape {
+    @Override
+    public boolean isChanneled() {
+        return false;
+    }
+
+    @Override
+    public float manaCostMultiplier(ItemStack spellStack) {
+        return 2.5f;
+    }
+
+    @Override
+    public boolean isTerminusShape() {
+        return false;
+    }
+
+    @Override
+    public boolean isPrincipumShape() {
+        return true;
+    }
+
+    @Override
+    public SpellCastResult beginStackStage(SpellItem item, ItemStack stack, LivingEntity caster, LivingEntity target, World world, double x, double y, double z, Direction side, boolean giveXP, int useCount) {
+        if (world.isRemote) return SpellCastResult.SUCCESS;
+//        EntitySpellEffect wall = new EntitySpellEffect(world);
+//        wall.setRadius(SpellUtils.getModifiedIntMul(3, stack, caster, target, world, SpellModifiers.RADIUS));
+//        wall.setTicksToExist(SpellUtils.getModifiedDoubleAdd(0, stack, caster, target, world, SpellModifiers.GRAVITY));
+//        wall.setGravity(SpellUtils.getModifiedIntMul(100, stack, caster, target, world, SpellModifiers.DURATION));
+//        wall.SetCasterAndStack(caster, stack);
+//        wall.setPosition(x, y, z);
+//        wall.setWall(caster.rotationYaw);
+//        world.addEntity(wall);
+        return SpellCastResult.SUCCESS;
+    }
+
+    @Override
+    public ISpellIngredient[] getRecipe() {
+        return new ISpellIngredient[]{
+                new ItemStackSpellIngredient(new ItemStack(ModItems.VINTEUM.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.MAGIC_WALL.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.MAGIC_WALL.get())),
+                new ItemStackSpellIngredient(new ItemStack(Items.COBBLESTONE_WALL)),
+                new ItemStackSpellIngredient(new ItemStack(Items.OAK_FENCE)),
+                new EssenceSpellIngredient(EssenceType.ANY, 2500)
+        };
+    }
+
+    @Override
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
+
+    }
+
+    @Override
+    public EnumSet<SpellModifiers> getModifiers() {
+        return EnumSet.of(SpellModifiers.RADIUS, SpellModifiers.GRAVITY, SpellModifiers.DURATION, SpellModifiers.COLOR, SpellModifiers.TARGET_NONSOLID_BLOCKS);
+    }
+}
