c0b93be4c1f904265cc9d4d100b42ca54874650a
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-11-28 19:38:56 +0100

    Add speed modifier

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/modifier/Speed.java
@@ -0,0 +1,43 @@
+package minecraftschurli.arsmagicalegacy.objects.spell.modifier;
+
+import minecraftschurli.arsmagicalegacy.api.spellsystem.*;
+import minecraftschurli.arsmagicalegacy.init.*;
+import net.minecraft.entity.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.potion.*;
+import net.minecraft.world.*;
+
+import java.util.*;
+
+public class Speed extends SpellModifier {
+    @Override
+    public EnumSet<SpellModifiers> getAspectsModified() {
+        return EnumSet.of(SpellModifiers.SPEED);
+    }
+
+    @Override
+    public float getModifier(SpellModifiers type, LivingEntity caster, Entity target, World world, CompoundNBT nbt) {
+        return 2.6f;
+    }
+
+    @Override
+    public float getManaCostMultiplier(ItemStack spellStack, int stage, int quantity) {
+        return 1.15f * quantity;
+    }
+
+    @Override
+    public ISpellIngredient[] getRecipe() {
+        return new ISpellIngredient[]{
+                new ItemStackSpellIngredient(new ItemStack(ModItems.LIGHTNING_ESSENCE.get())),
+                new ItemStackSpellIngredient(new ItemStack(Items.LEATHER_BOOTS)),
+                new ItemStackSpellIngredient(new ItemStack(Items.CARROT)),
+                new ItemStackSpellIngredient(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potion.getPotionTypeForName("swiftness")))
+        };
+    }
+
+    @Override
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
+
+    }
+}
