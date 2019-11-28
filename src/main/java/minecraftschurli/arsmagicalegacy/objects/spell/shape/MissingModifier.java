fa7b2d44fbf945cfe50e8d9994f39e4a2ef133bf
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-11-28 17:31:11 +0100

    Add missing modifier

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/shape/MissingModifier.java
@@ -0,0 +1,36 @@
+package minecraftschurli.arsmagicalegacy.objects.spell.shape;
+
+import minecraftschurli.arsmagicalegacy.api.spellsystem.*;
+import net.minecraft.entity.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.world.*;
+
+import java.util.*;
+
+public class MissingModifier extends SpellModifier {
+    @Override
+    public EnumSet<SpellModifiers> getAspectsModified() {
+        return EnumSet.noneOf(SpellModifiers.class);
+    }
+
+    @Override
+    public float getModifier(SpellModifiers type, LivingEntity caster, Entity target, World world, CompoundNBT nbt) {
+        return 0;
+    }
+
+    @Override
+    public float getManaCostMultiplier(ItemStack spellStack, int stage, int quantity) {
+        return 1;
+    }
+
+    @Override
+    public ISpellIngredient[] getRecipe() {
+        return new ISpellIngredient[0];
+    }
+
+    @Override
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
+
+    }
+}
