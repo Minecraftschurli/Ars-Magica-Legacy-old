2e09ae24982fafbe3e9f818632c1eb3872a43609
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-11-28 19:55:19 +0100

    Add VelocityAdded modifier

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/modifier/VelocityAdded.java
@@ -0,0 +1,42 @@
+package minecraftschurli.arsmagicalegacy.objects.spell.modifier;
+
+import minecraftschurli.arsmagicalegacy.api.spellsystem.*;
+import minecraftschurli.arsmagicalegacy.init.*;
+import net.minecraft.entity.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.world.*;
+
+import java.util.*;
+
+public class VelocityAdded extends SpellModifier {
+    @Override
+    public EnumSet<SpellModifiers> getAspectsModified() {
+        return EnumSet.of(SpellModifiers.VELOCITY_ADDED);
+    }
+
+    @Override
+    public float getModifier(SpellModifiers type, LivingEntity caster, Entity target, World world, CompoundNBT nbt) {
+        return 0.5f;
+    }
+
+    @Override
+    public float getManaCostMultiplier(ItemStack spellStack, int stage, int quantity) {
+        return 1.3f * quantity;
+    }
+
+    @Override
+    public ISpellIngredient[] getRecipe() {
+        return new ISpellIngredient[]{
+                new ItemStackSpellIngredient(new ItemStack(ModItems.ICE_ESSENCE.get())),
+                new ItemStackSpellIngredient(new ItemStack(Items.FEATHER)),
+                new ItemStackSpellIngredient(new ItemStack(Items.MINECART)),
+                new ItemStackSpellIngredient(new ItemStack(Items.OAK_BOAT))
+        };
+    }
+
+    @Override
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
+
+    }
+}
