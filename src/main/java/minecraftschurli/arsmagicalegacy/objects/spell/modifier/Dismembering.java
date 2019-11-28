587c1e92d196adaf0bc75e9d42dd5dc7c2d739d6
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-11-28 17:18:17 +0100

    Add dismembering modifier

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/modifier/Dismembering.java
@@ -0,0 +1,39 @@
+package minecraftschurli.arsmagicalegacy.objects.spell.modifier;
+
+import minecraftschurli.arsmagicalegacy.api.spellsystem.*;
+import net.minecraft.entity.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.world.*;
+
+import java.util.*;
+
+public class Dismembering extends SpellModifier {
+    @Override
+    public EnumSet<SpellModifiers> getAspectsModified() {
+        return EnumSet.of(SpellModifiers.DISMEMBERING_LEVEL);
+    }
+
+    @Override
+    public float getModifier(SpellModifiers type, LivingEntity caster, Entity target, World world, CompoundNBT nbt) {
+        return 0.05f;
+    }
+
+    @Override
+    public float getManaCostMultiplier(ItemStack spellStack, int stage, int quantity) {
+        return 1.25f * quantity;
+    }
+
+    @Override
+    public ISpellIngredient[] getRecipe() {
+        return new ISpellIngredient[]{
+                new ItemStackSpellIngredient(new ItemStack(Items.BONE)),
+                new ItemStackSpellIngredient(new ItemStack(Items.WITHER_SKELETON_SKULL))
+        };
+    }
+
+    @Override
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
+
+    }
+}
