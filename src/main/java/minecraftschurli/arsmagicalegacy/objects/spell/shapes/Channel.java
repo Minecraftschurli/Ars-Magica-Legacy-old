9e3f81de9b4339f63c67c942442d6daf4c888f46
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-11-28 10:26:07 +0100

    Add channel shape

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/shapes/Channel.java
@@ -0,0 +1,62 @@
+package minecraftschurli.arsmagicalegacy.objects.spell.shapes;
+
+import minecraftschurli.arsmagicalegacy.api.spellsystem.*;
+import minecraftschurli.arsmagicalegacy.init.*;
+import minecraftschurli.arsmagicalegacy.objects.item.*;
+import minecraftschurli.arsmagicalegacy.objects.spell.*;
+import minecraftschurli.arsmagicalegacy.util.*;
+import net.minecraft.entity.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.util.*;
+import net.minecraft.world.*;
+
+import java.util.*;
+
+public class Channel extends SpellShape {
+    @Override
+    public boolean isChanneled() {
+        return true;
+    }
+
+    @Override
+    public float manaCostMultiplier(ItemStack spellStack) {
+        return 1;
+    }
+
+    @Override
+    public boolean isTerminusShape() {
+        return true;
+    }
+
+    @Override
+    public boolean isPrincipumShape() {
+        return false;
+    }
+
+    @Override
+    public SpellCastResult beginStackStage(SpellItem item, ItemStack stack, LivingEntity caster, LivingEntity target, World world, double x, double y, double z, Direction side, boolean giveXP, int useCount) {
+        if (useCount % 10 == 0/* || SpellUtils.componentIsPresent(stack, Telekinesis.class) || SpellUtils.componentIsPresent(stack, Attract.class) || SpellUtils.componentIsPresent(stack, Repel.class)*/) return SpellUtils.applyStageToEntity(stack, caster, world, caster, giveXP);
+        return SpellCastResult.SUCCESS;
+    }
+
+    @Override
+    public ISpellIngredient[] getRecipe() {
+        return new ISpellIngredient[]{
+                new ItemStackSpellIngredient(new ItemStack(ModItems.ARCANE_ASH.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.TARMA_ROOT.get())),
+                new EssenceSpellIngredient(EssenceType.ANY, 500)
+//                AffinityShiftUtils.getEssenceForAffinity(Affinity.ARCANE),
+        };
+    }
+
+    @Override
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
+
+    }
+
+    @Override
+    public EnumSet<SpellModifiers> getModifiers() {
+        return EnumSet.noneOf(SpellModifiers.class);
+    }
+}
