f6781a322f3cb76282a8d6b8a1fd6665912a1eeb
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-11-28 19:20:37 +0100

    Add rune procs modifier

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/modifier/RuneProcs.java
@@ -0,0 +1,43 @@
+package minecraftschurli.arsmagicalegacy.objects.spell.modifier;
+
+import minecraftschurli.arsmagicalegacy.api.spellsystem.*;
+import minecraftschurli.arsmagicalegacy.init.*;
+import net.minecraft.entity.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.world.*;
+
+import java.util.*;
+
+public class RuneProcs extends SpellModifier {
+    @Override
+    public EnumSet<SpellModifiers> getAspectsModified() {
+        return EnumSet.of(SpellModifiers.PROCS);
+    }
+
+    @Override
+    public float getModifier(SpellModifiers type, LivingEntity caster, Entity target, World world, CompoundNBT nbt) {
+        return 4;
+    }
+
+    @Override
+    public float getManaCostMultiplier(ItemStack spellStack, int stage, int quantity) {
+        return 1.65f * quantity;
+    }
+
+    @Override
+    public ISpellIngredient[] getRecipe() {
+        return new ISpellIngredient[]{
+                new ItemStackSpellIngredient(new ItemStack(ModItems.ARCANE_ESSENCE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.BLACK_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.BLUE_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.PURPLE_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.WHITE_RUNE.get()))
+        };
+    }
+
+    @Override
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
+
+    }
+}
