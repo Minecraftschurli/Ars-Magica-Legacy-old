752d7d61b460394f1ff1f397a8c70f499418cb02
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-03 15:42:45 +0100

    Add createwater modifier

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/CreateWater.java
@@ -0,0 +1,71 @@
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
+
+import minecraftschurli.arsmagicalegacy.api.spell.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
+import minecraftschurli.arsmagicalegacy.init.*;
+import net.minecraft.block.*;
+import net.minecraft.entity.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.particles.*;
+import net.minecraft.util.*;
+import net.minecraft.util.math.*;
+import net.minecraft.world.*;
+
+import java.util.*;
+
+public class CreateWater extends SpellComponent {
+    @Override
+    public boolean applyEffectBlock(ItemStack stack, World world, BlockPos blockPos, Direction blockFace, double impactX, double impactY, double impactZ, LivingEntity caster) {
+        Block block = world.getBlockState(blockPos).getBlock();
+        if (block == Blocks.CAULDRON){
+            world.setBlockState(blockPos, Blocks.CAULDRON.getDefaultState().with(CauldronBlock.LEVEL, 3));
+            return true;
+        }
+        blockPos = blockPos.offset(blockFace);
+        block = world.getBlockState(blockPos).getBlock();
+        if (world.isAirBlock(blockPos) || block == Blocks.SNOW || block == Blocks.WATER || block instanceof FlowerBlock) {
+            world.setBlockState(blockPos, Blocks.WATER.getDefaultState());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean applyEffectEntity(ItemStack stack, World world, LivingEntity caster, Entity target) {
+        return false;
+    }
+
+    @Override
+    public float getManaCost(LivingEntity caster) {
+        return 25;
+    }
+
+    @Override
+    public ItemStack[] getReagents(LivingEntity caster) {
+        return new ItemStack[0];
+    }
+
+    @Override
+    public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
+        for (int i = 0; i < 15; ++i) world.addParticle(ParticleTypes.SPLASH, x - 0.5 + rand.nextDouble(), y, z - 0.5 + rand.nextDouble(), 0.5 - rand.nextDouble(), 0.1, 0.5 - rand.nextDouble());
+    }
+
+    @Override
+    public ISpellIngredient[] getRecipe() {
+        return new ISpellIngredient[]{
+                new ItemStackSpellIngredient(new ItemStack(ModItems.BLUE_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(Items.WATER_BUCKET))
+        };
+    }
+
+    @Override
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
+
+    }
+
+    @Override
+    public EnumSet<SpellModifiers> getModifiers() {
+        return EnumSet.noneOf(SpellModifiers.class);
+    }
+}
