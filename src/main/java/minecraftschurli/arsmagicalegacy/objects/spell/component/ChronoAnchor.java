2aafc0c33a322bc1d1437902c460217ff76e19b1
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-03 10:47:24 +0100

    Add chronoanchor modifier

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/ChronoAnchor.java
@@ -0,0 +1,78 @@
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
+
+import minecraftschurli.arsmagicalegacy.api.spell.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
+import minecraftschurli.arsmagicalegacy.init.*;
+import minecraftschurli.arsmagicalegacy.util.*;
+import net.minecraft.entity.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.util.*;
+import net.minecraft.util.math.*;
+import net.minecraft.world.*;
+
+import java.util.*;
+
+public class ChronoAnchor extends SpellComponent {
+    @Override
+    public boolean applyEffectBlock(ItemStack stack, World world, BlockPos blockPos, Direction blockFace, double impactX, double impactY, double impactZ, LivingEntity caster) {
+        return false;
+    }
+
+    @Override
+    public boolean applyEffectEntity(ItemStack stack, World world, LivingEntity caster, Entity target) {
+        if (target instanceof LivingEntity){
+            int duration = SpellUtils.getModifiedIntMul(ModEffects.DEFAULT_BUFF_DURATION, stack, caster, target, world, SpellModifiers.DURATION);
+            if (!world.isRemote) {
+//                if (((LivingEntity)target).getActivePotionEffects().contains(BuffEffectTemporalAnchor.class)) ((LivingEntity)target).getActivePotionEffects().remove(BuffEffectTemporalAnchor.class);
+//                ((LivingEntity) target).addPotionEffect(new BuffEffectTemporalAnchor(duration, SpellUtils.countModifiers(SpellModifiers.BUFF_POWER, stack)));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public float getManaCost(LivingEntity caster) {
+        return 80;
+    }
+
+    @Override
+    public ItemStack[] getReagents(LivingEntity caster) {
+        return new ItemStack[0];
+    }
+
+    @Override
+    public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
+        for (int i = 0; i < 25; ++i){
+//            AMParticle particle = (AMParticle)ArsMagica2.proxy.particleManager.spawn(world, "clock", x, y, z);
+//            if (particle != null){
+//                particle.addRandomOffset(1, 2, 1);
+//                particle.setParticleScale(0.1f);
+//                particle.AddParticleController(new ParticleOrbitEntity(particle, target, 0.1f + rand.nextFloat() * 0.1f, 1, false));
+//                particle.AddParticleController(new ParticleFadeOut(particle, 1, false).setFadeSpeed(0.05f));
+//                particle.setMaxAge(40);
+//                if (colorModifier > -1) particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
+//            }
+        }
+    }
+
+    @Override
+    public ISpellIngredient[] getRecipe() {
+        return new ISpellIngredient[]{
+                new ItemStackSpellIngredient(new ItemStack(ModItems.WHITE_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(Items.CLOCK)),
+                new ItemStackSpellIngredient(new ItemStack(Items.NETHER_STAR))
+        };
+    }
+
+    @Override
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
+
+    }
+
+    @Override
+    public EnumSet<SpellModifiers> getModifiers() {
+        return EnumSet.of(SpellModifiers.DURATION, SpellModifiers.BUFF_POWER);
+    }
+}
