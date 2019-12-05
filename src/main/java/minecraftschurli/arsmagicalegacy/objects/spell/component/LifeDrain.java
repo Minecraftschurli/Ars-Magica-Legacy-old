8865a03efb3315ab659dd039fa1d50dbff9a60c8
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-05 10:50:00 +0100

    Add LifeDrain.java

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/LifeDrain.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/LifeDrain.java
@@ -1,12 +1,9 @@
-package minecraftschurli.arsmagicalegacy.spell.component;
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
 
-import com.google.common.collect.*;
-import minecraftschurli.arsmagicalegacy.api.affinity.*;
 import minecraftschurli.arsmagicalegacy.api.spell.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
 import minecraftschurli.arsmagicalegacy.init.*;
-import minecraftschurli.arsmagicalegacy.items.*;
-import minecraftschurli.arsmagicalegacy.particles.*;
-import minecraftschurli.arsmagicalegacy.utils.*;
+import minecraftschurli.arsmagicalegacy.util.*;
 import net.minecraft.entity.*;
 import net.minecraft.item.*;
 import net.minecraft.nbt.*;
@@ -24,9 +21,7 @@ public class LifeDrain extends SpellComponent {
 
     @Override
     public boolean applyEffectEntity(ItemStack stack, World world, LivingEntity caster, Entity target) {
-        if (world.isRemote || !(target instanceof LivingEntity) || ((LivingEntity) target).getCreatureAttribute() == EnumCreatureAttribute.UNDEAD) {
-            return true;
-        }
+        if (world.isRemote || !(target instanceof LivingEntity) || ((LivingEntity) target).isEntityUndead()) return true;
         int magnitude = SpellUtils.getModifiedIntAdd(4, stack, caster, target, world, SpellModifiers.DAMAGE);
         boolean success = SpellUtils.attackTargetSpecial(stack, target, DamageSource.causeIndirectMagicDamage(caster, caster), SpellUtils.modifyDamage(caster, magnitude));
         if (success) {
@@ -42,24 +37,22 @@ public class LifeDrain extends SpellComponent {
     }
 
     @Override
-    public ItemStack[] reagents(LivingEntity caster) {
+    public ItemStack[] getReagents(LivingEntity caster) {
         return null;
     }
 
     @Override
     public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
         for (int i = 0; i < 15; ++i) {
-            AMParticle particle = (AMParticle) ArsMagica2.proxy.particleManager.spawn(world, "ember", x, y, z);
-            if (particle != null) {
-                particle.addRandomOffset(1, 1, 1);
-                particle.setIgnoreMaxAge(true);
-                particle.AddParticleController(new ParticleArcToEntity(particle, 1, caster, false).SetSpeed(0.03f).generateControlPoints());
-                particle.setRGBColorF(1, 0.2f, 0.2f);
-                particle.SetParticleAlpha(0.5f);
-                if (colorModifier > -1) {
-                    particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
-                }
-            }
+//            AMParticle particle = (AMParticle) ArsMagicaLegacy.proxy.particleManager.spawn(world, "ember", x, y, z);
+//            if (particle != null) {
+//                particle.addRandomOffset(1, 1, 1);
+//                particle.setIgnoreMaxAge(true);
+//                particle.AddParticleController(new ParticleArcToEntity(particle, 1, caster, false).SetSpeed(0.03f).generateControlPoints());
+//                particle.setRGBColorF(1, 0.2f, 0.2f);
+//                particle.SetParticleAlpha(0.5f);
+//                if (colorModifier > -1) particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
+//            }
         }
     }
 
@@ -68,26 +61,26 @@ public class LifeDrain extends SpellComponent {
         return EnumSet.of(SpellModifiers.DAMAGE);
     }
 
-    @Override
-    public Set<Affinity> getAffinity() {
-        return Sets.newHashSet(Affinity.LIFE);
-    }
-
+//    @Override
+//    public Set<Affinity> getAffinity() {
+//        return Sets.newHashSet(Affinity.LIFE);
+//    }
+//
     @Override
     public ISpellIngredient[] getRecipe() {
         return new ISpellIngredient[]{
-                new ItemStack(ModItems.BLACK_RUNE.get()),
-                new ItemStack(ModItems.itemOre, 1, ItemOre.META_SUNSTONE),
-                //TODO BlocksCommonProxy.aum
+                new ItemStackSpellIngredient(new ItemStack(ModItems.BLACK_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.SUNSTONE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.AUM.get()))
         };
     }
 
+//    @Override
+//    public float getAffinityShift(Affinity affinity) {
+//        return 0.01f;
+//    }
+//
     @Override
-    public float getAffinityShift(Affinity affinity) {
-        return 0.01f;
-    }
-
-    @Override
-    public void encodeBasicData(CompoundNBT tag, Object[] recipe) {
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
     }
 }
