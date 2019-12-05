1cd2b865948dbfedf9af7934f36c09ca6a721215
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-05 10:42:53 +0100

    Add Summon.java

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/Summon.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/Summon.java
@@ -1,14 +1,12 @@
-package minecraftschurli.arsmagicalegacy.spell.component;
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
 
-import com.google.common.collect.*;
-import minecraftschurli.arsmagicalegacy.api.affinity.*;
 import minecraftschurli.arsmagicalegacy.api.spell.*;
-import minecraftschurli.arsmagicalegacy.extensions.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
 import minecraftschurli.arsmagicalegacy.init.*;
-import minecraftschurli.arsmagicalegacy.items.*;
-import minecraftschurli.arsmagicalegacy.power.*;
-import minecraftschurli.arsmagicalegacy.utils.*;
+import minecraftschurli.arsmagicalegacy.objects.spell.*;
+import minecraftschurli.arsmagicalegacy.util.*;
 import net.minecraft.entity.*;
+import net.minecraft.entity.monster.*;
 import net.minecraft.entity.player.*;
 import net.minecraft.item.*;
 import net.minecraft.nbt.*;
@@ -18,33 +16,21 @@ import net.minecraft.world.*;
 
 import java.util.*;
 
-@SuppressWarnings("deprecation")
+@SuppressWarnings("deprecated")
 public class Summon extends SpellComponent {
-    public EntityLiving summonCreature(ItemStack stack, LivingEntity caster, LivingEntity target, World world, double x, double y, double z) {
-        Class<? extends Entity> clazz = getSummonType(stack);
-        EntityLiving entity = null;
+    public LivingEntity summonCreature(ItemStack stack, LivingEntity caster, LivingEntity target, World world, double x, double y, double z) {
+        LivingEntity entity = null;
         try {
-            entity = (EntityLiving) clazz.getConstructor(World.class).newInstance(world);
+            entity = new SkeletonEntity(EntityType.SKELETON, world);
         } catch (Throwable t) {
             t.printStackTrace();
             return null;
         }
-        if (entity == null) {
-            return null;
-        }
-        if (entity instanceof EntitySkeleton) {
-            ((EntitySkeleton) entity).setSkeletonType(SkeletonType.NORMAL);
-            ((EntitySkeleton) entity).setHeldItem(EnumHand.MAIN_HAND, new ItemStack(Items.BOW));
-        } else if (entity instanceof EntityHorse && caster instanceof PlayerEntity) {
-            ((EntityHorse) entity).setTamedBy(((PlayerEntity) caster));
-        }
+        entity.setHeldItem(Hand.MAIN_HAND, new ItemStack(Items.BOW));
         entity.setPosition(x, y, z);
         world.addEntity(entity);
-        if (caster instanceof PlayerEntity) {
-            EntityUtils.makeSummon_PlayerFaction((CreatureEntity) entity, (PlayerEntity) caster, false);
-        } else {
-            EntityUtils.makeSummon_MonsterFaction((CreatureEntity) entity, false);
-        }
+        if (caster instanceof PlayerEntity) EntityUtils.makeSummonPlayerFaction((CreatureEntity) entity, (PlayerEntity) caster, false);
+        else EntityUtils.makeSummonMonsterFaction((CreatureEntity) entity, false);
         EntityUtils.setOwner(entity, caster);
         int duration = SpellUtils.getModifiedIntMul(4800, stack, caster, target, world, SpellModifiers.DURATION);
         EntityUtils.setSummonDuration(entity, duration);
@@ -59,84 +45,30 @@ public class Summon extends SpellComponent {
 
     @Override
     public ISpellIngredient[] getRecipe() {
-        //Chimerite, purified vinteum, blue orchid, monster focus, any filled crystal phylactery, 1500 dark power
         return new ISpellIngredient[]{
-                new ItemStack(ModItems.itemOre, 1, ItemOre.META_CHIMERITE),
-                new ItemStack(ModItems.itemOre, 1, ItemOre.META_PURIFIED_VINTEUM),
-                ModBlocks.cerublossom,
-                ModItems.mobFocus,
-                new ItemStack(ModItems.crystalPhylactery, 1, ItemCrystalPhylactery.META_FULL),
-                "E:" + PowerTypes.DARK.ID(), 1500
+                new ItemStackSpellIngredient(new ItemStack(ModItems.CHIMERITE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.PURIFIED_VINTEUM.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.CERUBLOSSOM.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.MONSTER_FOCUS.get())),
+                new EssenceSpellIngredient(EssenceType.DARK, 1500)
         };
-    }
-
-    public void setSummonType(CompoundNBT stack, ItemStack phylacteryStack) {
-        if (phylacteryStack.getItemDamage() == ItemCrystalPhylactery.META_FULL && phylacteryStack.getItem() instanceof ItemCrystalPhylactery) {
-            setSummonType(stack, ModItems.crystalPhylactery.getSpawnClass(phylacteryStack));
-        }
-    }
-
-    public Class<? extends Entity> getSummonType(ItemStack stack) {
-        String s = SpellUtils.getSpellMetadata(stack, "SummonType");
-        if (s == null || s == "")
-            s = "Skeleton"; //default!  default!  default!
-        Class<? extends Entity> clazz = (Class<? extends Entity>) EntityList.NAME_TO_CLASS.get(s);
-        return clazz;
-    }
-
-    public void setSummonType(CompoundNBT stack, String s) {
-        Class<? extends Entity> clazz = (Class<? extends Entity>) EntityList.NAME_TO_CLASS.get(s);
-        setSummonType(stack, clazz);
-    }
-
-    public void setSummonType(CompoundNBT stack, Class<? extends Entity> clazz) {
-        clazz = checkForSpecialSpawns(stack, clazz);
-        String s = (String) EntityList.CLASS_TO_NAME.get(clazz);
-        if (s == null)
-            s = "";
-        SpellUtils.setSpellMetadata(stack, "SpawnClassName", s);
-        SpellUtils.setSpellMetadata(stack, "SummonType", s);
-    }
-
-    private Class<? extends Entity> checkForSpecialSpawns(CompoundNBT tag, Class<? extends Entity> clazz) {
-//		if (clazz == EntityChicken.class){
-//			if (SpellUtils.modifierIsPresent(SpellModifiers.DAMAGE, stack) && SpellUtils.componentIsPresent(stack, Haste.class)){
-//				return EntityBattleChicken.class;
-//			}
-//		}else if (clazz == EntityCow.class){
-//			if (SpellUtils.modifierIsPresent(SpellModifiers.DAMAGE, stack) && SpellUtils.componentIsPresent(stack, AstralDistortion.class)){
-//				return EntityHellCow.class;
-//			}
-//		}
-        return clazz;
     }
 
     @Override
     public boolean applyEffectBlock(ItemStack stack, World world, BlockPos blockPos, Direction blockFace, double impactX, double impactY, double impactZ, LivingEntity caster) {
         if (!world.isRemote) {
-            if (EntityExtension.For(caster).getCanHaveMoreSummons()) {
-                return summonCreature(stack, caster, caster, world, impactX, impactY, impactZ) != null;
-            } else {
-                if (caster instanceof PlayerEntity) {
-                    ((PlayerEntity) caster).addChatMessage(new TextComponentString(I18n.translateToLocal("minecraftschurli.arsmagicalegacy.tooltip.noMoreSummons")));
-                }
-            }
+//            if (EntityExtension.For(caster).getCanHaveMoreSummons()) return summonCreature(stack, caster, caster, world, impactX, impactY, impactZ) != null;
+//            else if (caster instanceof PlayerEntity) caster.sendMessage(new TranslationTextComponent("minecraftschurli.arsmagicalegacy.tooltip.noMoreSummons"));
         }
         return true;
     }
 
     @Override
     public boolean applyEffectEntity(ItemStack stack, World world, LivingEntity caster, Entity target) {
-        if (target instanceof LivingEntity && EntityUtils.isSummon((LivingEntity) target))
-            return false;
+        if (target instanceof LivingEntity && EntityUtils.isSummon((LivingEntity) target)) return false;
         if (!world.isRemote) {
-            if (EntityExtension.For(caster).getCanHaveMoreSummons()) {
-                return summonCreature(stack, caster, caster, world, target.posX, target.posY, target.posZ) != null;
-            } else {
-                if (caster instanceof PlayerEntity) {
-                    ((PlayerEntity) caster).addChatComponentMessage(new TextComponentString(I18n.translateToLocal("minecraftschurli.arsmagicalegacy.tooltip.noMoreSummons")));
-                }
-            }
+//            if (EntityExtension.For(caster).getCanHaveMoreSummons()) return summonCreature(stack, caster, caster, world, target.posX, target.posY, target.posZ) != null;
+//            else if (caster instanceof PlayerEntity) caster.sendMessage(new TranslationTextComponent("minecraftschurli.arsmagicalegacy.tooltip.noMoreSummons"));
         }
         return true;
     }
@@ -147,32 +79,26 @@ public class Summon extends SpellComponent {
     }
 
     @Override
-    public ItemStack[] reagents(LivingEntity caster) {
-        return null;
+    public ItemStack[] getReagents(LivingEntity caster) {
+        return new ItemStack[0];
     }
 
     @Override
     public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
+
     }
 
+    //    @Override
+//    public Set<Affinity> getAffinity() {
+//        return Sets.newHashSet(Affinity.ENDER, Affinity.LIFE);
+//    }
+//
+//    @Override
+//    public float getAffinityShift(Affinity affinity) {
+//        return 0.01f;
+//    }
+//
     @Override
-    public Set<Affinity> getAffinity() {
-        return Sets.newHashSet(Affinity.ENDER, Affinity.LIFE);
-    }
-
-    @Override
-    public float getAffinityShift(Affinity affinity) {
-        return 0.01f;
-    }
-
-    @Override
-    public void encodeBasicData(CompoundNBT tag, Object[] recipe) {
-        for (Object obj : recipe) {
-            if (obj instanceof ItemStack) {
-                ItemStack is = (ItemStack) obj;
-                if (is.getItem().equals(ModItems.crystalPhylactery))
-                    setSummonType(tag, is);
-            }
-        }
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
     }
 }
