ddbb143aebc579fb1d61364dd7329a87c5d56a96
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-05 10:43:02 +0100

    Add Storm.java

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/Storm.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/Storm.java
@@ -1,15 +1,12 @@
-package minecraftschurli.arsmagicalegacy.spell.component;
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
 
-import com.google.common.collect.*;
-import minecraftschurli.arsmagicalegacy.api.affinity.*;
 import minecraftschurli.arsmagicalegacy.api.spell.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
 import minecraftschurli.arsmagicalegacy.init.*;
-import minecraftschurli.arsmagicalegacy.items.*;
-import minecraftschurli.arsmagicalegacy.particles.*;
+import net.minecraft.block.*;
 import net.minecraft.entity.*;
-import net.minecraft.entity.monster.*;
+import net.minecraft.entity.effect.*;
 import net.minecraft.entity.player.*;
-import net.minecraft.init.*;
 import net.minecraft.item.*;
 import net.minecraft.nbt.*;
 import net.minecraft.util.*;
@@ -46,34 +43,22 @@ public class Storm extends SpellComponent {
                     int randPosX = (int) caster.posX + world.rand.nextInt(xzradius * 2) - xzradius;
                     int randPosZ = (int) caster.posZ + world.rand.nextInt(xzradius * 2) - xzradius;
                     int posY = (int) caster.posY;
-                    while (!world.canBlockSeeSky(new BlockPos(randPosX, posY, randPosZ))) {
-                        posY++;
-                    }
-                    while (world.getBlockState(new BlockPos(randPosX, posY - 1, randPosZ)).getBlock().equals(Blocks.AIR)) {
-                        posY--;
-                    }
-                    EntityLightningBolt bolt = new EntityLightningBolt(world, randPosX, posY, randPosZ, false);
-                    world.addWeatherEffect(bolt);
+                    while (!world.canBlockSeeSky(new BlockPos(randPosX, posY, randPosZ))) posY++;
+                    while (world.getBlockState(new BlockPos(randPosX, posY - 1, randPosZ)).getBlock().equals(Blocks.AIR)) posY--;
+                    LightningBoltEntity bolt = new LightningBoltEntity(world, randPosX, posY, randPosZ, false);
+                    world.addEntity(bolt);
                 } else if (random < 80) {
-                    List<Entity> entities = world.getEntitiesWithinAABB(Entity.class, caster.getEntityBoundingBox().expand(xzradius, 10D, xzradius), IMob.MOB_SELECTOR);
-                    if (entities.size() <= 0) {
-                        return;
-                    }
+                    List<Entity> entities = world.getEntitiesWithinAABB(Entity.class, caster.getBoundingBox().expand(xzradius, 10D, xzradius));
+                    if (entities.size() <= 0) return;
                     Entity target = entities.get(world.rand.nextInt(entities.size()));
                     if (target != null && world.canBlockSeeSky(target.getPosition())) {
-                        if (caster instanceof PlayerEntity) {
-                            target.attackEntityFrom(DamageSource.causePlayerDamage((PlayerEntity) caster), 1);
-                        }
-                        EntityLightningBolt bolt = new EntityLightningBolt(world, target.posX, target.posY, target.posZ, false);
-                        world.addWeatherEffect(bolt);
+                        if (caster instanceof PlayerEntity) target.attackEntityFrom(DamageSource.causePlayerDamage((PlayerEntity) caster), 1);
+                        LightningBoltEntity bolt = new LightningBoltEntity(world, target.posX, target.posY, target.posZ, false);
+                        world.addEntity(bolt);
                     }
                 }
             }
-        } else {
-            if (!world.isRemote) {
-                world.getWorldInfo().setRaining(true);
-            }
-        }
+        } else if (!world.isRemote) world.getWorldInfo().setRaining(true);
     }
 
     @Override
@@ -82,43 +67,41 @@ public class Storm extends SpellComponent {
     }
 
     @Override
-    public ItemStack[] reagents(LivingEntity caster) {
+    public ItemStack[] getReagents(LivingEntity caster) {
         return null;
     }
 
     @Override
     public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
-        AMParticle particle = (AMParticle) ArsMagica2.proxy.particleManager.spawn(world, "symbols", x, y - 1, z);
-        if (particle != null) {
-            particle.AddParticleController(new ParticleOrbitEntity(particle, target, 0.2f, 2, false).SetTargetDistance(1));
-            particle.setMaxAge(40);
-            particle.setParticleScale(0.1f);
-            if (colorModifier > -1) {
-                particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
-            }
-        }
+//        AMParticle particle = (AMParticle) ArsMagicaLegacy.proxy.particleManager.spawn(world, "symbols", x, y - 1, z);
+//        if (particle != null) {
+//            particle.AddParticleController(new ParticleOrbitEntity(particle, target, 0.2f, 2, false).SetTargetDistance(1));
+//            particle.setMaxAge(40);
+//            particle.setParticleScale(0.1f);
+//            if (colorModifier > -1) particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
+//        }
     }
 
-    @Override
-    public Set<Affinity> getAffinity() {
-        return Sets.newHashSet(Affinity.LIGHTNING, Affinity.NATURE);
-    }
-
+//    @Override
+//    public Set<Affinity> getAffinity() {
+//        return Sets.newHashSet(Affinity.LIGHTNING, Affinity.NATURE);
+//    }
+//
     @Override
     public ISpellIngredient[] getRecipe() {
         return new ISpellIngredient[]{
-                new ItemStack(ModItems.YELLOW_RUNE.get()),
-                new ItemStack(ModItems.itemOre, 1, ItemOre.META_BLUE_TOPAZ),
-                Items.GHAST_TEAR
+                new ItemStackSpellIngredient(new ItemStack(ModItems.YELLOW_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.TOPAZ.get())),
+                new ItemStackSpellIngredient(new ItemStack(Items.GHAST_TEAR))
         };
     }
 
+//    @Override
+//    public float getAffinityShift(Affinity affinity) {
+//        return 0.00001f;
+//    }
+//
     @Override
-    public float getAffinityShift(Affinity affinity) {
-        return 0.00001f;
-    }
-
-    @Override
-    public void encodeBasicData(CompoundNBT tag, Object[] recipe) {
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
     }
 }
