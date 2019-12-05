eb3c5717c9982cd4eb54c0258458e485534885c2
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-05 11:10:50 +0100

    Add FireRain.java

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/FireRain.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/FireRain.java
@@ -1,14 +1,9 @@
-package minecraftschurli.arsmagicalegacy.spell.component;
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
 
-import com.google.common.collect.*;
-import minecraftschurli.arsmagicalegacy.api.affinity.*;
 import minecraftschurli.arsmagicalegacy.api.spell.*;
-import minecraftschurli.arsmagicalegacy.entity.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
 import minecraftschurli.arsmagicalegacy.init.*;
-import minecraftschurli.arsmagicalegacy.items.*;
-import minecraftschurli.arsmagicalegacy.utils.*;
 import net.minecraft.entity.*;
-import net.minecraft.init.*;
 import net.minecraft.item.*;
 import net.minecraft.nbt.*;
 import net.minecraft.util.*;
@@ -21,32 +16,26 @@ public class FireRain extends SpellComponent {
     @Override
     public ISpellIngredient[] getRecipe() {
         return new ISpellIngredient[]{
-                new ItemStack(ModItems.FIRE_ESSENCE.get()),
-                new ItemStack(ModItems.itemOre, 1, ItemOre.META_ARCANEASH),
-                Blocks.NETHERRACK,
-                new ItemStack(ModItems.FIRE_ESSENCE.get()),
-                Items.LAVA_BUCKET
+                new ItemStackSpellIngredient(new ItemStack(ModItems.FIRE_ESSENCE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.FIRE_ESSENCE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.ARCANE_ASH.get())),
+                new ItemStackSpellIngredient(new ItemStack(Items.LAVA_BUCKET)),
+                new ItemStackSpellIngredient(new ItemStack(Items.NETHERRACK))
         };
     }
 
     private boolean spawnFireRain(ItemStack stack, World world, LivingEntity caster, Entity target, double x, double y, double z) {
-        List<EntitySpellEffect> zones = world.getEntitiesWithinAABB(EntitySpellEffect.class, new AxisAlignedBB(x - 10, y - 10, z - 10, x + 10, y + 10, z + 10));
-        for (EntitySpellEffect zone : zones) {
-            if (zone.isRainOfFire())
-                return false;
-        }
+//        List<EntitySpellEffect> zones = world.getEntitiesWithinAABB(EntitySpellEffect.class, new AxisAlignedBB(x - 10, y - 10, z - 10, x + 10, y + 10, z + 10));
+//        for (EntitySpellEffect zone : zones) if (zone.isRainOfFire()) return false;
         if (!world.isRemote) {
-            int radius = SpellUtils.getModifiedIntAdd(2, stack, caster, target, world, SpellModifiers.RADIUS) / 2 + 1;
-            double damage = SpellUtils.getModifiedDoubleMul(1, stack, caster, target, world, SpellModifiers.DAMAGE);
-            int duration = SpellUtils.getModifiedIntMul(100, stack, caster, target, world, SpellModifiers.DURATION);
-            EntitySpellEffect fire = new EntitySpellEffect(world);
-            fire.setPosition(x, y, z);
-            fire.setRainOfFire(false);
-            fire.setRadius(radius);
-            fire.setDamageBonus((float) damage);
-            fire.setTicksToExist(duration);
-            fire.SetCasterAndStack(caster, stack);
-            world.addEntity(fire);
+//            EntitySpellEffect fire = new EntitySpellEffect(world);
+//            fire.setPosition(x, y, z);
+//            fire.setRainOfFire(false);
+//            fire.setRadius(SpellUtils.getModifiedIntAdd(2, stack, caster, target, world, SpellModifiers.RADIUS) / 2 + 1);
+//            fire.setDamageBonus(SpellUtils.getModifiedDoubleMul(1, stack, caster, target, world, SpellModifiers.DAMAGE));
+//            fire.setTicksToExist(SpellUtils.getModifiedIntMul(100, stack, caster, target, world, SpellModifiers.DURATION));
+//            fire.SetCasterAndStack(caster, stack);
+//            world.addEntity(fire);
         }
         return true;
     }
@@ -72,7 +61,7 @@ public class FireRain extends SpellComponent {
     }
 
     @Override
-    public ItemStack[] reagents(LivingEntity caster) {
+    public ItemStack[] getReagents(LivingEntity caster) {
         return null;
     }
 
@@ -80,17 +69,17 @@ public class FireRain extends SpellComponent {
     public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
     }
 
+//    @Override
+//    public Set<Affinity> getAffinity() {
+//        return Sets.newHashSet(Affinity.FIRE);
+//    }
+//
+//    @Override
+//    public float getAffinityShift(Affinity affinity) {
+//        return 0.1f;
+//    }
+//
     @Override
-    public Set<Affinity> getAffinity() {
-        return Sets.newHashSet(Affinity.FIRE);
-    }
-
-    @Override
-    public float getAffinityShift(Affinity affinity) {
-        return 0.1f;
-    }
-
-    @Override
-    public void encodeBasicData(CompoundNBT tag, Object[] recipe) {
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
     }
 }
