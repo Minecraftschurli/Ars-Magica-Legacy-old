65d3c1a3e9403e027e27e7170bd780891ed8c192
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-05 11:08:47 +0100

    Add HarvestPlants.java

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/HarvestPlants.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/HarvestPlants.java
@@ -1,12 +1,11 @@
-package minecraftschurli.arsmagicalegacy.spell.component;
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
 
-import com.google.common.collect.*;
-import minecraftschurli.arsmagicalegacy.api.affinity.*;
 import minecraftschurli.arsmagicalegacy.api.spell.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
 import minecraftschurli.arsmagicalegacy.init.*;
-import minecraftschurli.arsmagicalegacy.particles.*;
 import net.minecraft.block.*;
 import net.minecraft.entity.*;
+import net.minecraft.entity.player.*;
 import net.minecraft.item.*;
 import net.minecraft.nbt.*;
 import net.minecraft.util.*;
@@ -21,8 +20,8 @@ public class HarvestPlants extends SpellComponent {
         BlockState block = world.getBlockState(pos);
         if (!(block.getBlock() instanceof IGrowable)) return false;
         if (!world.isRemote) {
-            block.getBlock().breakBlock(world, pos, block);
-            block.getBlock().dropBlockAsItem(world, pos, block, 0);
+            block.getBlock().harvestBlock(world, (PlayerEntity) caster, pos, block, null, stack);
+//            block.getBlock().dropBlockAsItem(world, pos, block, 0);
             world.setBlockState(pos, Blocks.AIR.getDefaultState());
         }
         return true;
@@ -44,47 +43,45 @@ public class HarvestPlants extends SpellComponent {
     }
 
     @Override
-    public ItemStack[] reagents(LivingEntity caster) {
+    public ItemStack[] getReagents(LivingEntity caster) {
         return null;
     }
 
     @Override
     public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
         for (int i = 0; i < 25; ++i) {
-            AMParticle particle = (AMParticle) ArsMagica2.proxy.particleManager.spawn(world, "plant", x, y + 1, z);
-            if (particle != null) {
-                particle.addRandomOffset(1, 1, 1);
-                particle.AddParticleController(new ParticleFloatUpward(particle, 0, 0.3f, 1, false));
-                particle.setAffectedByGravity();
-                particle.setMaxAge(20);
-                particle.setParticleScale(0.1f);
-                particle.setRGBColorF(0.7f, 0.2f, 0.1f);
-                if (colorModifier > -1) {
-                    particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
-                }
-            }
+//            AMParticle particle = (AMParticle) ArsMagicaLegacy.proxy.particleManager.spawn(world, "plant", x, y + 1, z);
+//            if (particle != null) {
+//                particle.addRandomOffset(1, 1, 1);
+//                particle.AddParticleController(new ParticleFloatUpward(particle, 0, 0.3f, 1, false));
+//                particle.setAffectedByGravity();
+//                particle.setMaxAge(20);
+//                particle.setParticleScale(0.1f);
+//                particle.setRGBColorF(0.7f, 0.2f, 0.1f);
+//                if (colorModifier > -1) particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
+//            }
         }
     }
 
-    @Override
-    public Set<Affinity> getAffinity() {
-        return Sets.newHashSet(Affinity.NATURE);
-    }
-
+//    @Override
+//    public Set<Affinity> getAffinity() {
+//        return Sets.newHashSet(Affinity.NATURE);
+//    }
+//
     @Override
     public ISpellIngredient[] getRecipe() {
         return new ISpellIngredient[]{
-                new ItemStack(ModItems.GREEN_RUNE.get()),
-                Items.SHEARS
+                new ItemStackSpellIngredient(new ItemStack(ModItems.GREEN_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(Items.SHEARS))
         };
     }
 
+//    @Override
+//    public float getAffinityShift(Affinity affinity) {
+//        return 0.02f;
+//    }
+//
     @Override
-    public float getAffinityShift(Affinity affinity) {
-        return 0.02f;
-    }
-
-    @Override
-    public void encodeBasicData(CompoundNBT tag, Object[] recipe) {
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
     }
 }
