48622de54cb61dfe2e40f1db3ebff2dce41dff8d
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-05 10:49:48 +0100

    Add LifeTap.java

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/LifeTap.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/LifeTap.java
@@ -1,18 +1,11 @@
-package minecraftschurli.arsmagicalegacy.spell.component;
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
 
-import com.google.common.collect.*;
-import minecraftschurli.arsmagicalegacy.api.affinity.*;
-import minecraftschurli.arsmagicalegacy.api.blocks.*;
-import minecraftschurli.arsmagicalegacy.api.extensions.*;
-import minecraftschurli.arsmagicalegacy.api.rituals.*;
 import minecraftschurli.arsmagicalegacy.api.spell.*;
-import minecraftschurli.arsmagicalegacy.extensions.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
 import minecraftschurli.arsmagicalegacy.init.*;
-import minecraftschurli.arsmagicalegacy.particles.*;
-import minecraftschurli.arsmagicalegacy.utils.*;
+import minecraftschurli.arsmagicalegacy.util.*;
+import net.minecraft.block.*;
 import net.minecraft.entity.*;
-import net.minecraft.entity.item.*;
-import net.minecraft.init.*;
 import net.minecraft.item.*;
 import net.minecraft.nbt.*;
 import net.minecraft.util.*;
@@ -24,21 +17,20 @@ import java.util.*;
 public class LifeTap extends SpellComponent {
     @Override
     public boolean applyEffectBlock(ItemStack stack, World world, BlockPos pos, Direction blockFace, double impactX, double impactY, double impactZ, LivingEntity caster) {
-        if (world.getBlockState(pos).getBlock().equals(Blocks.MOB_SPAWNER)) {
-            boolean hasMatch = RitualShapeHelper.instance.matchesRitual(this, world, pos);
-            if (hasMatch) {
-                if (!world.isRemote) {
-                    world.setBlockToAir(pos);
-                    RitualShapeHelper.instance.consumeReagents(this, world, pos);
-                    RitualShapeHelper.instance.consumeShape(this, world, pos);
-                    ItemEntity item = new ItemEntity(world);
-                    item.setPosition(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
-                    item.setItemEntityStack(new ItemStack(ModBlocks.inertSpawner));
-                    world.addEntity(item);
-                } else {
-                }
-                return true;
-            }
+        if (world.getBlockState(pos).getBlock().equals(Blocks.SPAWNER)) {
+//            boolean hasMatch = RitualShapeHelper.instance.matchesRitual(this, world, pos);
+//            if (hasMatch) {
+//                if (!world.isRemote) {
+//                    world.setBlockToAir(pos);
+//                    RitualShapeHelper.instance.consumeReagents(this, world, pos);
+//                    RitualShapeHelper.instance.consumeShape(this, world, pos);
+//                    ItemEntity item = new ItemEntity(world);
+//                    item.setPosition(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
+//                    item.setItemEntityStack(new ItemStack(ModBlocks.inertSpawner));
+//                    world.addEntity(item);
+//                }
+//                return true;
+//            }
         }
         return false;
     }
@@ -48,13 +40,10 @@ public class LifeTap extends SpellComponent {
         if (!(target instanceof LivingEntity)) return false;
         if (!world.isRemote) {
             double damage = SpellUtils.getModifiedDoubleMul(2, stack, caster, target, world, SpellModifiers.DAMAGE);
-            IEntityExtension casterProperties = EntityExtension.For(caster);
-            float manaRefunded = (float) (((damage * 0.01)) * casterProperties.getMaxMana());
-            if ((caster).attackEntityFrom(DamageSource.outOfWorld, (int) Math.floor(damage))) {
-                casterProperties.setCurrentMana(casterProperties.getCurrentMana() + manaRefunded);
-            } else {
-                return false;
-            }
+////            IEntityExtension casterProperties = EntityExtension.For(caster);
+////            float manaRefunded = (float) (((damage * 0.01)) * casterProperties.getMaxMana());
+////            if ((caster).attackEntityFrom(DamageSource.outOfWorld, (int) Math.floor(damage))) casterProperties.setCurrentMana(casterProperties.getCurrentMana() + manaRefunded);
+//            else return false;
         }
         return true;
     }
@@ -70,73 +59,58 @@ public class LifeTap extends SpellComponent {
     }
 
     @Override
-    public ItemStack[] reagents(LivingEntity caster) {
-        return null;
-    }
-
-    @Override
     public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
         for (int i = 0; i < 25; ++i) {
-            AMParticle particle = (AMParticle) ArsMagica2.proxy.particleManager.spawn(world, "sparkle2", x, y, z);
-            if (particle != null) {
-                particle.addRandomOffset(2, 2, 2);
-                particle.setMaxAge(15);
-                particle.setParticleScale(0.1f);
-                particle.AddParticleController(new ParticleApproachEntity(particle, target, 0.1, 0.1, 1, false));
-                if (rand.nextBoolean())
-                    particle.setRGBColorF(0.4f, 0.1f, 0.5f);
-                else
-                    particle.setRGBColorF(0.1f, 0.5f, 0.1f);
-                if (colorModifier > -1) {
-                    particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
-                }
-            }
+//            AMParticle particle = (AMParticle) ArsMagicaLegacy.proxy.particleManager.spawn(world, "sparkle2", x, y, z);
+//            if (particle != null) {
+//                particle.addRandomOffset(2, 2, 2);
+//                particle.setMaxAge(15);
+//                particle.setParticleScale(0.1f);
+//                particle.AddParticleController(new ParticleApproachEntity(particle, target, 0.1, 0.1, 1, false));
+//                if (rand.nextBoolean()) particle.setRGBColorF(0.4f, 0.1f, 0.5f);
+//                else particle.setRGBColorF(0.1f, 0.5f, 0.1f);
+//                if (colorModifier > -1) particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
+//            }
         }
     }
 
-    @Override
-    public Set<Affinity> getAffinity() {
-        return Sets.newHashSet(Affinity.LIFE, Affinity.ENDER);
-    }
-
+//    @Override
+//    public Set<Affinity> getAffinity() {
+//        return Sets.newHashSet(Affinity.LIFE, Affinity.ENDER);
+//    }
+//
     @Override
     public ISpellIngredient[] getRecipe() {
         return new ISpellIngredient[]{
-                new ItemStack(ModItems.BLACK_RUNE.get()),
-                ModBlocks.aum
+                new ItemStackSpellIngredient(new ItemStack(ModItems.BLACK_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.AUM.get())),
         };
     }
 
+//    @Override
+//    public float getAffinityShift(Affinity affinity) {
+//        return 0.01f;
+//    }
+//
+//    @Override
+//    public MultiblockStructureDefinition getRitualShape() {
+//        return RitualShapeHelper.instance.corruption;
+//    }
+//
     @Override
-    public float getAffinityShift(Affinity affinity) {
-        return 0.01f;
-    }
-
-    @Override
-    public MultiblockStructureDefinition getRitualShape() {
-        return RitualShapeHelper.instance.corruption;
-    }
-
-    @Override
-    public ItemStack[] getReagents() {
+    public ItemStack[] getReagents(LivingEntity caster) {
         return new ItemStack[]{
-                new ItemStack(ModItems.mobFocus),
+                new ItemStack(ModItems.MONSTER_FOCUS.get()),
                 new ItemStack(ModItems.ENDER_ESSENCE.get())
         };
     }
 
+//    @Override
+//    public int getReagentSearchRadius() {
+//        return 3;
+//    }
+//
     @Override
-    public int getReagentSearchRadius() {
-        return 3;
-    }
-
-    @Override
-    public void encodeBasicData(CompoundNBT tag, Object[] recipe) {
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public ItemStack getResult() {
-        return new ItemStack(ModBlocks.inertSpawner);
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
     }
 }
