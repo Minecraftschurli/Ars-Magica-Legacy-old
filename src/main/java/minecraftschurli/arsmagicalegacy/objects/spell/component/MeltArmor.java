492954a6e3aad36e04a6d9fae4de071339e769ee
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-05 10:47:45 +0100

    Add MeltArmor.java

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/MeltArmor.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/MeltArmor.java
@@ -1,12 +1,9 @@
-package minecraftschurli.arsmagicalegacy.spell.component;
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
 
-import com.google.common.collect.*;
-import minecraftschurli.arsmagicalegacy.api.affinity.*;
 import minecraftschurli.arsmagicalegacy.api.spell.*;
-import minecraftschurli.arsmagicalegacy.particles.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
 import net.minecraft.entity.*;
 import net.minecraft.entity.player.*;
-import net.minecraft.init.*;
 import net.minecraft.item.*;
 import net.minecraft.nbt.*;
 import net.minecraft.util.*;
@@ -22,7 +19,7 @@ public class MeltArmor extends SpellComponent {
     @Override
     public ISpellIngredient[] getRecipe() {
         return new ISpellIngredient[]{
-                new ItemStack(Blocks.SPONGE)
+                new ItemStackSpellIngredient(new ItemStack(Items.SPONGE))
         };
     }
 
@@ -35,33 +32,23 @@ public class MeltArmor extends SpellComponent {
         return false;
     }
 
-    private void doMeltArmor(LivingEntity caster, ItemStack[] armor) {
-        double mmpsCharge = getMMPSCharge(armor);
+    private void doMeltArmor(LivingEntity caster, NonNullList<ItemStack> armor) {
+        double mmpsCharge = -1;
+        for (ItemStack stack : armor) if (stack != null && stack.getTag() != null) {
+                CompoundNBT subCompound = (CompoundNBT) stack.getTag().get(mmpsNBTTagName);
+                if (subCompound != null && subCompound.get(mmpsChargeTagName) != null) mmpsCharge += subCompound.getDouble(mmpsChargeTagName);
+        }
         for (ItemStack stack : armor) {
             if (stack == null) continue;
-            if (!stack.hasTagCompound() || !stack.getTagCompound().hasKey(mmpsNBTTagName)) {
-                stack.damageItem((int) Math.ceil(stack.getItem().getMaxDamage() * 0.25f), caster);
-            } else {
-                CompoundNBT subCompound = (CompoundNBT) stack.getTagCompound().getTag(mmpsNBTTagName);
-                double charge = stack.getTagCompound().getDouble(mmpsChargeTagName);
+            if (!stack.hasTag() || stack.getTag().get(mmpsNBTTagName) == null) stack.damageItem((int) Math.ceil(stack.getItem().getMaxDamage() * 0.25f), caster, null);
+            else {
+                CompoundNBT subCompound = (CompoundNBT) stack.getTag().get(mmpsNBTTagName);
+                double charge = stack.getTag().getDouble(mmpsChargeTagName);
                 charge -= mmpsCharge * 0.75f;
                 if (charge < 0) charge = 0;
-                subCompound.setDouble(mmpsChargeTagName, charge);
+                subCompound.putDouble(mmpsChargeTagName, charge);
             }
         }
-    }
-
-    private double getMMPSCharge(ItemStack[] armor) {
-        double total = -1;
-        for (ItemStack stack : armor) {
-            if (stack != null && stack.hasTagCompound()) {
-                CompoundNBT subCompound = (CompoundNBT) stack.getTagCompound().getTag(mmpsNBTTagName);
-                if (subCompound != null && subCompound.hasKey(mmpsChargeTagName)) {
-                    total += subCompound.getDouble(mmpsChargeTagName);
-                }
-            }
-        }
-        return total;
     }
 
     @Override
@@ -70,7 +57,7 @@ public class MeltArmor extends SpellComponent {
     }
 
     @Override
-    public ItemStack[] reagents(LivingEntity caster) {
+    public ItemStack[] getReagents(LivingEntity caster) {
         return null;
     }
 
@@ -81,36 +68,33 @@ public class MeltArmor extends SpellComponent {
 
     @Override
     public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
-        AMParticle particle = (AMParticle) ArsMagica2.proxy.particleManager.spawn(world, "radiant", x + 0.5, y + 0.5, z + 0.5);
-        if (particle != null) {
-            particle.AddParticleController(new ParticleHoldPosition(particle, 20, 1, false));
-            particle.setMaxAge(20);
-            particle.setParticleScale(0.3f);
-            particle.setRGBColorF(0.7f, 0.4f, 0.2f);
-            particle.SetParticleAlpha(0.1f);
-            if (colorModifier > -1) {
-                particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
-            }
-        }
+//        AMParticle particle = (AMParticle) ArsMagicaLegacy.proxy.particleManager.spawn(world, "radiant", x + 0.5, y + 0.5, z + 0.5);
+//        if (particle != null) {
+//            particle.AddParticleController(new ParticleHoldPosition(particle, 20, 1, false));
+//            particle.setMaxAge(20);
+//            particle.setParticleScale(0.3f);
+//            particle.setRGBColorF(0.7f, 0.4f, 0.2f);
+//            particle.SetParticleAlpha(0.1f);
+//            if (colorModifier > -1) particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
+//        }
     }
 
+//    @Override
+//    public Set<Affinity> getAffinity() {
+//        return Sets.newHashSet(Affinity.FIRE);
+//    }
+//
+//    @Override
+//    public float getAffinityShift(Affinity affinity) {
+//        return 0;
+//    }
+//
     @Override
-    public Set<Affinity> getAffinity() {
-        return Sets.newHashSet(Affinity.FIRE);
-    }
-
-    @Override
-    public float getAffinityShift(Affinity affinity) {
-        return 0;
-    }
-
-    @Override
-    public boolean applyEffectBlock(ItemStack stack, World world, BlockPos blockPos, Direction blockFace,
-                                    double impactX, double impactY, double impactZ, LivingEntity caster) {
+    public boolean applyEffectBlock(ItemStack stack, World world, BlockPos blockPos, Direction blockFace, double impactX, double impactY, double impactZ, LivingEntity caster) {
         return false;
     }
 
     @Override
-    public void encodeBasicData(CompoundNBT tag, Object[] recipe) {
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
     }
 }
