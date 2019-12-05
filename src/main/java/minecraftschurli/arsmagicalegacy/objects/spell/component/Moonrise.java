0f573c7c6718600d4db14407e6cea87b3ee70698
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-05 10:47:31 +0100

    Add Moonrise.java

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/Moonrise.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/Moonrise.java
@@ -1,10 +1,8 @@
-package minecraftschurli.arsmagicalegacy.spell.component;
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
 
-import com.google.common.collect.*;
-import minecraftschurli.arsmagicalegacy.api.affinity.*;
 import minecraftschurli.arsmagicalegacy.api.spell.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
 import minecraftschurli.arsmagicalegacy.init.*;
-import minecraftschurli.arsmagicalegacy.items.*;
 import net.minecraft.entity.*;
 import net.minecraft.item.*;
 import net.minecraft.nbt.*;
@@ -18,31 +16,31 @@ public class Moonrise extends SpellComponent {
     @Override
     public ISpellIngredient[] getRecipe() {
         return new ISpellIngredient[]{
-                new ItemStack(ModItems.itemOre, 1, ItemOre.META_MOONSTONE),
-                Items.CLOCK,
-                new ItemStack(ModItems.core, 1, ItemCore.META_PURE)
+                new ItemStackSpellIngredient(new ItemStack(ModItems.MOONSTONE.get())),
+                new ItemStackSpellIngredient(new ItemStack(Items.CLOCK))
         };
     }
 
-    private boolean setNightTime(World world) {
-        if (!world.isDaytime())
-            return false;
+    @Override
+    public boolean applyEffectBlock(ItemStack stack, World world, BlockPos pos, Direction blockFace, double impactX, double impactY, double impactZ, LivingEntity caster) {
+        if (!world.isDaytime()) return false;
         if (!world.isRemote) {
-            long curTime = ((WorldServer) world).getWorldTime();
+            long curTime = world.getGameTime();
             int day = (int) Math.floor(curTime / 24000);
-            ((WorldServer) world).setWorldTime((day * 24000) + 13250);
+            world.setGameTime((day * 24000) + 13250);
         }
         return true;
     }
 
     @Override
-    public boolean applyEffectBlock(ItemStack stack, World world, BlockPos pos, Direction blockFace, double impactX, double impactY, double impactZ, LivingEntity caster) {
-        return setNightTime(world);
-    }
-
-    @Override
     public boolean applyEffectEntity(ItemStack stack, World world, LivingEntity caster, Entity target) {
-        return setNightTime(world);
+        if (!world.isDaytime()) return false;
+        if (!world.isRemote) {
+            long curTime = world.getGameTime();
+            int day = (int) Math.floor(curTime / 24000);
+            world.setGameTime((day * 24000) + 13250);
+        }
+        return true;
     }
 
     @Override
@@ -56,7 +54,7 @@ public class Moonrise extends SpellComponent {
     }
 
     @Override
-    public ItemStack[] reagents(LivingEntity caster) {
+    public ItemStack[] getReagents(LivingEntity caster) {
         return null;
     }
 
@@ -64,17 +62,17 @@ public class Moonrise extends SpellComponent {
     public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
     }
 
+//    @Override
+//    public Set<Affinity> getAffinity() {
+//        return Sets.newHashSet(Affinity.NONE);
+//    }
+//
+//    @Override
+//    public float getAffinityShift(Affinity affinity) {
+//        return 0;
+//    }
+//
     @Override
-    public Set<Affinity> getAffinity() {
-        return Sets.newHashSet(Affinity.NONE);
-    }
-
-    @Override
-    public float getAffinityShift(Affinity affinity) {
-        return 0;
-    }
-
-    @Override
-    public void encodeBasicData(CompoundNBT tag, Object[] recipe) {
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
     }
 }
