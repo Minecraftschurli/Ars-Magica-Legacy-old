f45aca9637882fe1fd5392103ff83bbf872ed719
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-05 10:50:11 +0100

    Add Levitation.java

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/Levitation.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/Levitation.java
@@ -1,17 +1,13 @@
-package minecraftschurli.arsmagicalegacy.spell.component;
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
 
-import com.google.common.collect.*;
-import minecraftschurli.arsmagicalegacy.api.affinity.*;
-import minecraftschurli.arsmagicalegacy.api.blocks.*;
-import minecraftschurli.arsmagicalegacy.api.rituals.*;
 import minecraftschurli.arsmagicalegacy.api.spell.*;
-import minecraftschurli.arsmagicalegacy.buffs.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
 import minecraftschurli.arsmagicalegacy.init.*;
-import minecraftschurli.arsmagicalegacy.particles.*;
-import minecraftschurli.arsmagicalegacy.utils.*;
+import minecraftschurli.arsmagicalegacy.util.*;
 import net.minecraft.entity.*;
 import net.minecraft.item.*;
 import net.minecraft.nbt.*;
+import net.minecraft.potion.*;
 import net.minecraft.util.*;
 import net.minecraft.util.math.*;
 import net.minecraft.world.*;
@@ -22,14 +18,12 @@ public class Levitation extends SpellComponent {
     @Override
     public boolean applyEffectEntity(ItemStack stack, World world, LivingEntity caster, Entity target) {
         if (target instanceof LivingEntity) {
-            int duration = SpellUtils.getModifiedIntMul(PotionEffectsDefs.default_buff_duration, stack, caster, target, world, SpellModifiers.DURATION);
-            //duration = SpellUtils.modifyDurationBasedOnArmor(caster, duration);
-            if (RitualShapeHelper.instance.matchesRitual(this, world, target.getPosition())) {
-                duration += (3600 * (SpellUtils.countModifiers(SpellModifiers.BUFF_POWER, stack) + 1));
-                RitualShapeHelper.instance.consumeReagents(this, world, target.getPosition());
-            }
-            if (!world.isRemote)
-                ((LivingEntity) target).addPotionEffect(new BuffEffectLevitation(duration, SpellUtils.countModifiers(SpellModifiers.BUFF_POWER, stack)));
+            int duration = SpellUtils.getModifiedIntMul(ModEffects.DEFAULT_BUFF_DURATION, stack, caster, target, world, SpellModifiers.DURATION);
+//            if (RitualShapeHelper.instance.matchesRitual(this, world, target.getPosition())) {
+//                duration += (3600 * (SpellUtils.countModifiers(SpellModifiers.BUFF_POWER, stack) + 1));
+//                RitualShapeHelper.instance.consumeReagents(this, world, target.getPosition());
+//            }
+            if (!world.isRemote) ((LivingEntity) target).addPotionEffect(new EffectInstance(Effects.LEVITATION, duration, SpellUtils.countModifiers(SpellModifiers.BUFF_POWER, stack)));
             return true;
         }
         return false;
@@ -41,11 +35,6 @@ public class Levitation extends SpellComponent {
     }
 
     @Override
-    public ItemStack[] reagents(LivingEntity caster) {
-        return null;
-    }
-
-    @Override
     public EnumSet<SpellModifiers> getModifiers() {
         return EnumSet.of(SpellModifiers.BUFF_POWER, SpellModifiers.DURATION);
     }
@@ -53,69 +42,60 @@ public class Levitation extends SpellComponent {
     @Override
     public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
         for (int i = 0; i < 15; ++i) {
-            AMParticle particle = (AMParticle) ArsMagica2.proxy.particleManager.spawn(world, "ember", x, y, z);
-            if (particle != null) {
-                particle.addRandomOffset(1, 0.5, 1);
-                particle.AddParticleController(new ParticleOrbitEntity(particle, target, 0.1f + rand.nextFloat() * 0.1f, 1, false).SetTargetDistance(0.4f + rand.nextFloat() * 0.2f));
-                particle.setMaxAge(40);
-                particle.setParticleScale(0.1f);
-                particle.setRGBColorF(0.2f, 0.2f, 0.6f);
-                if (colorModifier > -1) {
-                    particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
-                }
-            }
+//            AMParticle particle = (AMParticle) ArsMagicaLegacy.proxy.particleManager.spawn(world, "ember", x, y, z);
+//            if (particle != null) {
+//                particle.addRandomOffset(1, 0.5, 1);
+//                particle.AddParticleController(new ParticleOrbitEntity(particle, target, 0.1f + rand.nextFloat() * 0.1f, 1, false).SetTargetDistance(0.4f + rand.nextFloat() * 0.2f));
+//                particle.setMaxAge(40);
+//                particle.setParticleScale(0.1f);
+//                particle.setRGBColorF(0.2f, 0.2f, 0.6f)
+//                if (colorModifier > -1) particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
+//            }
         }
     }
 
-    @Override
-    public Set<Affinity> getAffinity() {
-        return Sets.newHashSet(Affinity.AIR);
-    }
-
+//    @Override
+//    public Set<Affinity> getAffinity() {
+//        return Sets.newHashSet(Affinity.AIR);
+//    }
+//
     @Override
     public ISpellIngredient[] getRecipe() {
         return new ISpellIngredient[]{
-                new ItemStack(ModItems.WHITE_RUNE.get()),
-                ModBlocks.tarmaRoot
+                new ItemStackSpellIngredient(new ItemStack(ModItems.WHITE_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(ModItems.TARMA_ROOT.get())),
         };
     }
 
+//    @Override
+//    public float getAffinityShift(Affinity affinity) {
+//        return 0.05f;
+//    }
+//
+//    @Override
+//    public MultiblockStructureDefinition getRitualShape() {
+//        return RitualShapeHelper.instance.hourglass;
+//    }
+//
     @Override
-    public float getAffinityShift(Affinity affinity) {
-        return 0.05f;
-    }
-
-    @Override
-    public MultiblockStructureDefinition getRitualShape() {
-        return RitualShapeHelper.instance.hourglass;
-    }
-
-    @Override
-    public ItemStack[] getReagents() {
+    public ItemStack[] getReagents(LivingEntity caster) {
         return new ItemStack[]{
                 new ItemStack(Items.FEATHER),
-                //TODO new ItemStack(ModBlocks.tarmaRoot)
+                new ItemStack(ModItems.TARMA_ROOT.get())
         };
     }
 
+//    @Override
+//    public int getReagentSearchRadius() {
+//        return 3;
+//    }
+//
     @Override
-    public int getReagentSearchRadius() {
-        return 3;
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
     }
 
     @Override
-    public void encodeBasicData(CompoundNBT tag, Object[] recipe) {
-    }
-
-    @Override
-    public boolean applyEffectBlock(ItemStack stack, World world, BlockPos blockPos, Direction blockFace,
-                                    double impactX, double impactY, double impactZ, LivingEntity caster) {
+    public boolean applyEffectBlock(ItemStack stack, World world, BlockPos blockPos, Direction blockFace, double impactX, double impactY, double impactZ, LivingEntity caster) {
         return false;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public ItemStack getResult() {
-        return null;
     }
 }
