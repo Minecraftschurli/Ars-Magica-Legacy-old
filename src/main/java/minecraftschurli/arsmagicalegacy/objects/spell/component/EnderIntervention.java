f934d54e794a525c21884c93f4d534d646b40254
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-05 11:11:44 +0100

    Add EnderIntervention.java

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/EnderIntervention.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/EnderIntervention.java
@@ -1,18 +1,15 @@
-package minecraftschurli.arsmagicalegacy.spell.component;
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
 
-import com.google.common.collect.*;
-import minecraftschurli.arsmagicalegacy.api.affinity.*;
 import minecraftschurli.arsmagicalegacy.api.spell.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
 import minecraftschurli.arsmagicalegacy.init.*;
-import minecraftschurli.arsmagicalegacy.particles.*;
-import minecraftschurli.arsmagicalegacy.utils.*;
 import net.minecraft.entity.*;
 import net.minecraft.entity.player.*;
-import net.minecraft.init.*;
 import net.minecraft.item.*;
 import net.minecraft.nbt.*;
 import net.minecraft.util.*;
 import net.minecraft.util.math.*;
+import net.minecraft.util.text.*;
 import net.minecraft.world.*;
 
 import java.util.*;
@@ -21,22 +18,19 @@ public class EnderIntervention extends SpellComponent {
     @Override
     public boolean applyEffectEntity(ItemStack stack, World world, LivingEntity caster, Entity target) {
         if (world.isRemote || !(target instanceof LivingEntity)) return true;
-        if (((LivingEntity) target).isPotionActive(PotionEffectsDefs.astralDistortion)) {
-            if (target instanceof PlayerEntity)
-                ((PlayerEntity) target).addChatMessage(new TextComponentString("The distortion around you prevents you from teleporting"));
+//        if (((LivingEntity) target).isPotionActive(ModEffects.ASTRAL_DISTORTION)) {
+//            if (target instanceof PlayerEntity) target.sendMessage(new TranslationTextComponent("The distortion around you prevents you from teleporting"));
+//            return true;
+//        }
+        if (target.dimension.getId() == 1) {
+            if (target instanceof PlayerEntity) target.sendMessage(new TranslationTextComponent("Nothing happens..."));
             return true;
-        }
-        if (target.dimension == 1) {
-            if (target instanceof PlayerEntity)
-                ((PlayerEntity) target).addChatMessage(new TextComponentString("Nothing happens..."));
-            return true;
-        } else if (target.dimension == -1) {
-            if (target instanceof PlayerEntity)
-                ((PlayerEntity) target).addChatMessage(new TextComponentString("You are already in the nether."));
+        } else if (target.dimension.getId() == -1) {
+            if (target instanceof PlayerEntity) target.sendMessage(new TranslationTextComponent("You are already in the nether."));
             return false;
         } else {
-            DimensionUtilities.doDimensionTransfer((LivingEntity) target, -1);
-            ArsMagica2.proxy.addDeferredDimensionTransfer((LivingEntity) target, -1);
+//            DimensionUtilities.doDimensionTransfer((LivingEntity) target, -1);
+//            ArsMagicaLegacy.proxy.addDeferredDimensionTransfer((LivingEntity) target, -1);
         }
         return true;
     }
@@ -52,54 +46,50 @@ public class EnderIntervention extends SpellComponent {
     }
 
     @Override
-    public ItemStack[] reagents(LivingEntity caster) {
+    public ItemStack[] getReagents(LivingEntity caster) {
         return new ItemStack[]{new ItemStack(ModItems.ENDER_ESSENCE.get())};
     }
 
     @Override
     public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
         for (int i = 0; i < 100; ++i) {
-            AMParticle particle = (AMParticle) ArsMagica2.proxy.particleManager.spawn(world, "ghost", x, y - 1, z);
-            if (particle != null) {
-                particle.addRandomOffset(1, 2, 1);
-                particle.AddParticleController(new ParticleFloatUpward(particle, 0, 0.1f, 1, false));
-                particle.setMaxAge(25 + rand.nextInt(10));
-                particle.setRGBColorF(0.7f, 0.2f, 0.2f);
-                if (colorModifier > -1) {
-                    particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
-                }
-            }
+//            AMParticle particle = (AMParticle) ArsMagicaLegacy.proxy.particleManager.spawn(world, "ghost", x, y - 1, z);
+//            if (particle != null) {
+//                particle.addRandomOffset(1, 2, 1);
+//                particle.AddParticleController(new ParticleFloatUpward(particle, 0, 0.1f, 1, false));
+//                particle.setMaxAge(25 + rand.nextInt(10));
+//                particle.setRGBColorF(0.7f, 0.2f, 0.2f);
+//                if (colorModifier > -1) particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
+//            }
         }
     }
 
-    @Override
-    public Set<Affinity> getAffinity() {
-        return Sets.newHashSet(Affinity.ENDER);
-    }
-
+//    @Override
+//    public Set<Affinity> getAffinity() {
+//        return Sets.newHashSet(Affinity.ENDER);
+//    }
+//
     @Override
     public ISpellIngredient[] getRecipe() {
         return new ISpellIngredient[]{
-                new ItemStack(ModItems.PURPLE_RUNE.get()),
-                Blocks.OBSIDIAN,
-                Blocks.OBSIDIAN,
-                Items.FLINT_AND_STEEL,
-                Items.ENDER_PEARL
+                new ItemStackSpellIngredient(new ItemStack(ModItems.PURPLE_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(Items.ENDER_PEARL)),
+                new ItemStackSpellIngredient(new ItemStack(Items.FLINT_AND_STEEL)),
+                new ItemStackSpellIngredient(new ItemStack(Items.OBSIDIAN, 2))
         };
     }
 
+//    @Override
+//    public float getAffinityShift(Affinity affinity) {
+//        return 0.4f;
+//    }
+//
     @Override
-    public float getAffinityShift(Affinity affinity) {
-        return 0.4f;
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
     }
 
     @Override
-    public void encodeBasicData(CompoundNBT tag, Object[] recipe) {
-    }
-
-    @Override
-    public boolean applyEffectBlock(ItemStack stack, World world, BlockPos blockPos, Direction blockFace,
-                                    double impactX, double impactY, double impactZ, LivingEntity caster) {
+    public boolean applyEffectBlock(ItemStack stack, World world, BlockPos blockPos, Direction blockFace, double impactX, double impactY, double impactZ, LivingEntity caster) {
         return false;
     }
 }
