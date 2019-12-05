f41d3dc4945237bf817a70afbdcef3e8ba44ada7
IchHabeHunger54 <kingdavidaut@gmail.com>
2019-12-05 10:38:04 +0100

    Add WateryGrave.java

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/WateryGrave.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/component/WateryGrave.java
@@ -1,21 +1,17 @@
-package minecraftschurli.arsmagicalegacy.spell.component;
+package minecraftschurli.arsmagicalegacy.objects.spell.component;
 
-import com.google.common.collect.*;
-import minecraftschurli.arsmagicalegacy.api.affinity.*;
-import minecraftschurli.arsmagicalegacy.api.blocks.*;
-import minecraftschurli.arsmagicalegacy.api.rituals.*;
 import minecraftschurli.arsmagicalegacy.api.spell.*;
-import minecraftschurli.arsmagicalegacy.buffs.*;
+import minecraftschurli.arsmagicalegacy.api.spell.crafting.*;
 import minecraftschurli.arsmagicalegacy.init.*;
-import minecraftschurli.arsmagicalegacy.particles.*;
-import minecraftschurli.arsmagicalegacy.utils.*;
+import minecraftschurli.arsmagicalegacy.util.*;
+import net.minecraft.block.*;
 import net.minecraft.entity.*;
-import net.minecraft.init.*;
 import net.minecraft.item.*;
 import net.minecraft.nbt.*;
 import net.minecraft.util.*;
 import net.minecraft.util.math.*;
 import net.minecraft.world.*;
+import net.minecraftforge.common.*;
 
 import java.util.*;
 
@@ -23,14 +19,12 @@ public class WateryGrave extends SpellComponent {
     @Override
     public boolean applyEffectEntity(ItemStack stack, World world, LivingEntity caster, Entity target) {
         if (target instanceof LivingEntity) {
-            int duration = SpellUtils.getModifiedIntMul(PotionEffectsDefs.default_buff_duration, stack, caster, target, world, SpellModifiers.DURATION);
-            //duration = SpellUtils.modifyDurationBasedOnArmor(caster, duration);
-            if (RitualShapeHelper.instance.matchesRitual(this, world, target.getPosition())) {
-                duration += (3600 * (SpellUtils.countModifiers(SpellModifiers.BUFF_POWER, stack) + 1));
-                RitualShapeHelper.instance.consumeReagents(this, world, target.getPosition());
-            }
-            if (!world.isRemote)
-                ((LivingEntity) target).addPotionEffect(new BuffEffectWateryGrave(duration, SpellUtils.countModifiers(SpellModifiers.BUFF_POWER, stack)));
+            int duration = SpellUtils.getModifiedIntMul(ModEffects.DEFAULT_BUFF_DURATION, stack, caster, target, world, SpellModifiers.DURATION);
+//            if (RitualShapeHelper.instance.matchesRitual(this, world, target.getPosition())) {
+//                duration += (3600 * (SpellUtils.countModifiers(SpellModifiers.BUFF_POWER, stack) + 1));
+//                RitualShapeHelper.instance.consumeReagents(this, world, target.getPosition());
+//            }
+//            if (!world.isRemote) ((LivingEntity) target).addPotionEffect(new WateryGraveEffect(duration, SpellUtils.countModifiers(SpellModifiers.BUFF_POWER, stack)));
             return true;
         }
         return false;
@@ -42,69 +36,59 @@ public class WateryGrave extends SpellComponent {
     }
 
     @Override
-    public ItemStack[] reagents(LivingEntity caster) {
-        return null;
-    }
-
-    @Override
     public void spawnParticles(World world, double x, double y, double z, LivingEntity caster, Entity target, Random rand, int colorModifier) {
         for (int i = 0; i < 25; ++i) {
-            AMParticle particle = (AMParticle) ArsMagica2.proxy.particleManager.spawn(world, "water_ball", x, y, z);
-            if (particle != null) {
-                particle.addRandomOffset(1, 2, 1);
-                particle.AddParticleController(new ParticleOrbitEntity(particle, target, 0.2f, 1, false));
-                particle.AddParticleController(new ParticleFadeOut(particle, 1, false).setFadeSpeed(0.05f).setKillParticleOnFinish(true));
-                particle.AddParticleController(new ParticleLeaveParticleTrail(particle, "water_ball", false, 5, 2, false)
-                        .addControllerToParticleList(new ParticleFloatUpward(particle, 0, -0.3f, 1, false))
-                        .setParticleRGB_F(1, 1, 1)
-                        .setTicksBetweenSpawns(2));
-                particle.setMaxAge(20);
-                particle.setParticleScale(0.01f);
-                if (colorModifier > -1) {
-                    particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
-                }
-            }
+//            AMParticle particle = (AMParticle) ArsMagicaLegacy.proxy.particleManager.spawn(world, "water_ball", x, y, z);
+//            if (particle != null) {
+//                particle.addRandomOffset(1, 2, 1);
+//                particle.AddParticleController(new ParticleOrbitEntity(particle, target, 0.2f, 1, false));
+//                particle.AddParticleController(new ParticleFadeOut(particle, 1, false).setFadeSpeed(0.05f).setKillParticleOnFinish(true));
+//                particle.AddParticleController(new ParticleLeaveParticleTrail(particle, "water_ball", false, 5, 2, false).addControllerToParticleList(new ParticleFloatUpward(particle, 0, -0.3f, 1, false)).setParticleRGB_F(1, 1, 1).setTicksBetweenSpawns(2));
+//                particle.setMaxAge(20);
+//                particle.setParticleScale(0.01f);
+//                if (colorModifier > -1) particle.setRGBColorF(((colorModifier >> 16) & 0xFF) / 255.0f, ((colorModifier >> 8) & 0xFF) / 255.0f, (colorModifier & 0xFF) / 255.0f);
+//            }
         }
     }
 
-    @Override
-    public Set<Affinity> getAffinity() {
-        return Sets.newHashSet(Affinity.WATER);
-    }
-
+//    @Override
+//    public Set<Affinity> getAffinity() {
+//        return Sets.newHashSet(Affinity.WATER);
+//    }
+//
     @Override
     public ISpellIngredient[] getRecipe() {
         return new ISpellIngredient[]{
-                new ItemStack(ModItems.BLUE_RUNE.get()),
-                Blocks.STONE,
-                Items.LEATHER_BOOTS
+                new ItemStackSpellIngredient(new ItemStack(ModItems.BLUE_RUNE.get())),
+                new ItemStackSpellIngredient(new ItemStack(Items.LEATHER_BOOTS)),
+                new ItemTagSpellIngredient(Tags.Items.STONE)
         };
     }
 
+//    @Override
+//    public float getAffinityShift(Affinity affinity) {
+//        return 0.05f;
+//    }
+//
+//    @Override
+//    public MultiblockStructureDefinition getRitualShape() {
+//        return RitualShapeHelper.instance.hourglass;
+//    }
+//
     @Override
-    public float getAffinityShift(Affinity affinity) {
-        return 0.05f;
-    }
-
-    @Override
-    public MultiblockStructureDefinition getRitualShape() {
-        return RitualShapeHelper.instance.hourglass;
-    }
-
-    @Override
-    public ItemStack[] getReagents() {
+    public ItemStack[] getReagents(LivingEntity caster) {
         return new ItemStack[]{
                 new ItemStack(Blocks.STONE)
         };
     }
 
+//    @Override
+//    public int getReagentSearchRadius() {
+//        return 3;
+//    }
+//
     @Override
-    public int getReagentSearchRadius() {
-        return 3;
-    }
-
-    @Override
-    public void encodeBasicData(CompoundNBT tag, Object[] recipe) {
+    public void encodeBasicData(CompoundNBT tag, ISpellIngredient[] recipe) {
     }
 
     @Override
@@ -115,11 +99,5 @@ public class WateryGrave extends SpellComponent {
     @Override
     public boolean applyEffectBlock(ItemStack stack, World world, BlockPos blockPos, Direction blockFace, double impactX, double impactY, double impactZ, LivingEntity caster) {
         return false;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public ItemStack getResult() {
-        return null;
     }
 }
