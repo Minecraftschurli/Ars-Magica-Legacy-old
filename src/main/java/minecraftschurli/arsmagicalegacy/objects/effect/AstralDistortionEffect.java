aa4b7fe5a01330986ca3ae9a5998e159c845d9ff
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 14:57:05 +0100

    add AstralDistortionEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/AstralDistortionEffect.java
@@ -0,0 +1,13 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import net.minecraft.potion.EffectType;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class AstralDistortionEffect extends AMEffect {
+    public AstralDistortionEffect() {
+        super(EffectType.HARMFUL, 0x6c0000);
+    }
+}
