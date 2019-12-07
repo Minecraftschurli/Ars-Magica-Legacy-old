a50386be411fcfa75da1f9d69849dbf1e4d8d18d
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 15:02:13 +0100

    add SpellReflectEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/SpellReflectEffect.java
@@ -0,0 +1,13 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import net.minecraft.potion.EffectType;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class SpellReflectEffect extends AMEffect {
+    public SpellReflectEffect() {
+        super(EffectType.BENEFICIAL, 0xadffff);
+    }
+}
