6a10a0e922fdd9b15938d9c3c59f5dc76b974a28
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 15:00:47 +0100

    add ScrambleSynapsesEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/ScrambleSynapsesEffect.java
@@ -0,0 +1,13 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import net.minecraft.potion.EffectType;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class ScrambleSynapsesEffect extends AMEffect {
+    public ScrambleSynapsesEffect() {
+        super(EffectType.HARMFUL, 0x306600);
+    }
+}
