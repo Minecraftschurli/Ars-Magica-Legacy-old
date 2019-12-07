612395c5022a4cefae09d041ed81453393eaa5a8
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 14:58:26 +0100

    add IlluminationEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/IlluminationEffect.java
@@ -0,0 +1,23 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.EffectType;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class IlluminationEffect extends AMEffect {
+    public IlluminationEffect() {
+        super(EffectType.BENEFICIAL, 0xffffbe);
+    }
+
+    @Override
+    public void performEffect(LivingEntity livingEntity, int amplifier) {
+        /*if (!livingEntity.world.isRemote && livingEntity.ticksExisted % 10 == 0) {
+            if (livingEntity.world.isAirBlock(livingEntity.getPosition()) && livingEntity.world.getLight(livingEntity.getPosition()) < 7){
+                livingEntity.world.setBlockState(livingEntity.getPosition(), ModBlocks.LIGHT.get().getDefaultState());
+            }
+        }*/
+    }
+}
