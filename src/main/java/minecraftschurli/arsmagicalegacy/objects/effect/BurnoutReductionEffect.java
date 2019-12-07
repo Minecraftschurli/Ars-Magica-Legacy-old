b3d0ee1f68e87de1dccce5333fde5e978ce4cb61
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 14:57:28 +0100

    add BurnoutReductionEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/BurnoutReductionEffect.java
@@ -0,0 +1,13 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import net.minecraft.potion.EffectType;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class BurnoutReductionEffect extends AMEffect {
+    public BurnoutReductionEffect() {
+        super(EffectType.BENEFICIAL, 0xcc0000);
+    }
+}
