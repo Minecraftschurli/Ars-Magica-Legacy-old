da25e7826e38118ae722d366360061f28147f2a7
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 14:57:49 +0100

    add ClarityEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/ClarityEffect.java
@@ -0,0 +1,13 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import net.minecraft.potion.EffectType;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class ClarityEffect extends AMEffect {
+    public ClarityEffect() {
+        super(EffectType.BENEFICIAL, 0xbbffff);
+    }
+}
