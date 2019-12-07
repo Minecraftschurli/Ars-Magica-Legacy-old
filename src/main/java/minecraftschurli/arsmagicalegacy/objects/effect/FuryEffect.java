edb1e2bb58359e42d7b2f6852c55389e5045c9e1
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 14:58:08 +0100

    add FuryEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/FuryEffect.java
@@ -0,0 +1,24 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.EffectType;
+import net.minecraft.potion.Effects;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class FuryEffect extends AMEffect {
+    public FuryEffect() {
+        super(EffectType.HARMFUL, 0xff8033);
+    }
+
+    @Override
+    public void stopEffect(LivingEntity livingEntity) {
+        if (!livingEntity.world.isRemote){
+            livingEntity.addPotionEffect(new EffectInstance(Effects.HUNGER, 200, 1));
+            livingEntity.addPotionEffect(new EffectInstance(Effects.NAUSEA, 200, 1));
+        }
+    }
+}
