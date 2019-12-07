a24121d57d633d616a332923c436f3ea196125eb
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 15:01:55 +0100

    add SilenceEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/SilenceEffect.java
@@ -0,0 +1,13 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import net.minecraft.potion.EffectType;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class SilenceEffect extends AMEffect {
+    public SilenceEffect() {
+        super(EffectType.HARMFUL, 0xc1c1ff);
+    }
+}
