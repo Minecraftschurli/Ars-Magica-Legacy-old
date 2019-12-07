745f3baef4cfac52f237fbae3ca36878bb17e6c9
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 14:58:44 +0100

    add InstantManaEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/InstantManaEffect.java
@@ -0,0 +1,22 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import minecraftschurli.arsmagicalegacy.util.MagicHelper;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.EffectType;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class InstantManaEffect extends AMInstantEffect {
+    public InstantManaEffect() {
+        super(EffectType.BENEFICIAL, 0x00ffff);
+    }
+
+    @Override
+    public void performEffect(@Nonnull LivingEntity entityLivingBaseIn, int amplifier) {
+        MagicHelper.getManaCapability(entityLivingBaseIn).increase(10 + (10*amplifier));
+    }
+}
