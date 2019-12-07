fc9c6954a743a969b697323fd1f656d2bd9e869d
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 15:01:19 +0100

    add ManaRegenEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/ManaRegenEffect.java
@@ -0,0 +1,13 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import net.minecraft.potion.EffectType;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class ManaRegenEffect extends AMEffect {
+    public ManaRegenEffect() {
+        super(EffectType.BENEFICIAL, 0x8bffff);
+    }
+}
