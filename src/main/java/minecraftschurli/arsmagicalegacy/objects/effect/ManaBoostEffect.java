ea473eaf63fca7a187798e2839cbd75a9738158b
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 15:00:16 +0100

    add ManaBoostEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/ManaBoostEffect.java
@@ -0,0 +1,13 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import net.minecraft.potion.EffectType;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class ManaBoostEffect extends AMEffect {
+    public ManaBoostEffect() {
+        super(EffectType.BENEFICIAL, 0x0093ff);
+    }
+}
