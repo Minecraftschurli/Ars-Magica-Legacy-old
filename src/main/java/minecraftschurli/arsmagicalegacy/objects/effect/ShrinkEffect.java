3f05a7b97d9521bb5a522b151a4c276b9923b1f2
Minecraftschurli <minecraftschurli@gmail.com>
2019-12-07 15:01:36 +0100

    add ShrinkEffect

---
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/effect/ShrinkEffect.java
@@ -0,0 +1,13 @@
+package minecraftschurli.arsmagicalegacy.objects.effect;
+
+import net.minecraft.potion.EffectType;
+
+/**
+ * @author Minecraftschurli
+ * @version 2019-12-07
+ */
+public class ShrinkEffect extends AMEffect {
+    public ShrinkEffect() {
+        super(EffectType.BENEFICIAL, 0x0000dd);
+    }
+}
