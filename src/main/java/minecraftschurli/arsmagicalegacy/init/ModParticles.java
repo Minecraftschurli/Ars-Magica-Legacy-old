b400dae3e2fa741a1c689eb06c6ebb7d96093521
IchHabeHunger54 <kingdavidaut@gmail.com>
2020-01-06 10:56:33 +0100

    Add particle registering

--- src/main/java/minecraftschurli/arsmagicalegacy/init/ModParticles.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/init/ModParticles.java
@@ -1,10 +1,8 @@
 package minecraftschurli.arsmagicalegacy.init;
 
-import minecraftschurli.arsmagicalegacy.*;
 import minecraftschurli.arsmagicalegacy.objects.particle.*;
 import net.minecraft.particles.*;
-import net.minecraft.util.registry.*;
-import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.*;
 
 /**
  * @author Minecraftschurli
@@ -12,6 +10,21 @@ import net.minecraftforge.fml.RegistryObject;
  */
 public class ModParticles implements IInit {
     public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> ARCANE = PARTICLE_TYPES.register("arcane", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> CLOCK = PARTICLE_TYPES.register("clock", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> EMBER = PARTICLE_TYPES.register("ember", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> GHOST = PARTICLE_TYPES.register("ghost", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> IMPLOSION = PARTICLE_TYPES.register("implosion", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> LENS_FLARE = PARTICLE_TYPES.register("lens_flare", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> LIGHT = PARTICLE_TYPES.register("light", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> PLANT = PARTICLE_TYPES.register("plant", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> PULSE = PARTICLE_TYPES.register("pulse", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> ROCK = PARTICLE_TYPES.register("rock", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> ROTATING_RINGS = PARTICLE_TYPES.register("rotating_rings", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> SHINY = PARTICLE_TYPES.register("shiny", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> SMOKE = PARTICLE_TYPES.register("smoke", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> SPARKLE = PARTICLE_TYPES.register("sparkle", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> WATERBALL = PARTICLE_TYPES.register("waterball", SimpleParticle.SimpleParticleType::new);
+    public static final RegistryObject<ParticleType<SimpleParticle.SimpleParticleType>> WIND = PARTICLE_TYPES.register("wind", SimpleParticle.SimpleParticleType::new);
 
     public static void register() {
     }
--- src/main/java/minecraftschurli/arsmagicalegacy/objects/particle/ParticleDispatcher.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/particle/ParticleDispatcher.java
@@ -1,24 +1,32 @@
 package minecraftschurli.arsmagicalegacy.objects.particle;
 
 import minecraftschurli.arsmagicalegacy.*;
-import minecraftschurli.arsmagicalegacy.init.ModParticles;
+import minecraftschurli.arsmagicalegacy.init.*;
 import net.minecraft.client.*;
-import net.minecraft.client.particle.IAnimatedSprite;
-import net.minecraft.client.particle.IParticleFactory;
-import net.minecraft.client.particle.Particle;
-import net.minecraft.client.particle.ParticleManager;
-import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.*;
 import net.minecraftforge.client.event.*;
 import net.minecraftforge.eventbus.api.*;
 import net.minecraftforge.fml.common.*;
-
-import javax.annotation.Nullable;
 
 @Mod.EventBusSubscriber(modid = ArsMagicaLegacy.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ParticleDispatcher {
     @SubscribeEvent
     public static void registerFactories(ParticleFactoryRegisterEvent e) {
         Minecraft.getInstance().particles.registerFactory(ModParticles.ARCANE.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.CLOCK.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.EMBER.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.GHOST.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.IMPLOSION.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.LENS_FLARE.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.LIGHT.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.PLANT.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.PULSE.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.ROCK.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.ROTATING_RINGS.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.SHINY.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.SMOKE.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.SPARKLE.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.WATERBALL.get(), SimpleParticle.Factory::new);
+        Minecraft.getInstance().particles.registerFactory(ModParticles.WIND.get(), SimpleParticle.Factory::new);
     }
 }
