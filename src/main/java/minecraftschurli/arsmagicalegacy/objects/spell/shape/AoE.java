fe658e10b1e13868ce59623816e89143268dd9b0
IchHabeHunger54 <kingdavidaut@gmail.com>
2020-01-20 10:55:46 +0100

    Utilities about particles

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/shape/AoE.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/spell/shape/AoE.java
@@ -117,10 +117,7 @@ public class AoE extends SpellShape {
             List<SpellModifier> mods = SpellUtils.getModifiersForStage(stack, -1);
             for (SpellModifier mod : mods) if (mod instanceof Color) color = (int) mod.getModifier(SpellModifiers.COLOR, null, null, null, stack.getTag());
         }
-        for (int i = 0; i < 360; i += /*ArsMagica2.config.FullGFX() ? 20 : ArsMagica2.config.LowGFX() ? 40 : */60) {
-                //TODO @Minecraftschurli fix this
-//                world.addParticle(new SimpleParticle().setColor(color), x, y + 1.5f, z, 0, 0, 0);
-        }
+        for (int i = 0; i < 360; i += /*ArsMagica2.config.FullGFX() ? 20 : ArsMagica2.config.LowGFX() ? 40 : */60) ParticleUtils.addParticle(world, ModParticles.LENS_FLARE, color, x, y, z);
     }
 
     private SpellCastResult applyStageHorizontal(ItemStack stack, LivingEntity caster, World world, BlockPos pos, Direction face, int radius, int gravityMagnitude, boolean giveXP) {
