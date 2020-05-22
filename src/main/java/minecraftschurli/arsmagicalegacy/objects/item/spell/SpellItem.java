4817c72ce353561cf5b0d6272107d7de95ff8e6d
Minecraftschurli <minecraftschurli@gmail.com>
2020-05-22 16:37:44 +0200

    fix spell casting
    Signed-off-by: Minecraftschurli <minecraftschurli@gmail.com>

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/item/spell/SpellItem.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/item/spell/SpellItem.java
@@ -1,10 +1,8 @@
 package minecraftschurli.arsmagicalegacy.objects.item.spell;
 
-import java.util.List;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import minecraftschurli.arsmagicalegacy.ArsMagicaLegacy;
 import minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
+import minecraftschurli.arsmagicalegacy.api.capability.CapabilityHelper;
 import minecraftschurli.arsmagicalegacy.api.spell.SpellCastResult;
 import minecraftschurli.arsmagicalegacy.api.spell.SpellShape;
 import minecraftschurli.arsmagicalegacy.util.SpellUtil;
@@ -14,15 +12,16 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Direction;
-import net.minecraft.util.Hand;
+import net.minecraft.util.*;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * @author Minecraftschurli
@@ -36,14 +35,35 @@ public class SpellItem extends Item {
 
     @Override
     @Nonnull
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, @Nonnull Hand handIn) {
-        playerIn.setActiveHand(handIn);
-        return new ActionResult<>(ActionResultType.SUCCESS, playerIn.getHeldItem(handIn));
+    public ActionResult<ItemStack> onItemRightClick(@Nonnull World worldIn, @Nonnull PlayerEntity playerIn, @Nonnull Hand handIn) {
+        ItemStack heldItem = playerIn.getHeldItem(handIn);
+        if (CapabilityHelper.getCurrentLevel(playerIn) < 0 && !playerIn.isCreative()) {
+            return ActionResult.resultFail(heldItem);
+        }
+        if (heldItem.hasTag()) {
+            String icon = heldItem.getTag().getString("icon");
+            if (!icon.isEmpty()) {
+                ResourceLocation iconLoc = ResourceLocation.tryCreate(icon);
+                if (iconLoc != null) {
+                    playerIn.setActiveHand(handIn);
+                    return new ActionResult<>(ActionResultType.SUCCESS, heldItem);
+                }
+            }
+        }
+        openIconPickGui(worldIn, playerIn, heldItem);
+        return new ActionResult<>(ActionResultType.SUCCESS, heldItem);
+    }
+
+    private void openIconPickGui(World worldIn, PlayerEntity playerIn, ItemStack heldItem) {
+        heldItem.getOrCreateTag().putString("icon", "arsmagicalegacy:shadow-spell");
     }
 
     @Override
     @Nonnull
     public ActionResultType onItemUse(ItemUseContext context) {
+        if (CapabilityHelper.getCurrentLevel(context.getPlayer()) < 0 && !context.getPlayer().isCreative()) {
+            return ActionResultType.FAIL;
+        }
         context.getPlayer().setActiveHand(context.getHand());
         return ActionResultType.SUCCESS;
     }
