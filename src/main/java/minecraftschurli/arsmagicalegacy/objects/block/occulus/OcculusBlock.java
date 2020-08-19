4d848397a20426b13b89fd480f4fd51d3584a5ea
IchHabeHunger54 <kingdavidaut@gmail.com>
2020-08-19 18:16:48 +0200

    voxel shape fix

--- src/main/java/minecraftschurli/arsmagicalegacy/objects/block/occulus/OcculusBlock.java
+++ src/main/java/minecraftschurli/arsmagicalegacy/objects/block/occulus/OcculusBlock.java
@@ -33,12 +33,12 @@ public class OcculusBlock extends Block {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     private static final VoxelShape SOCKET = BlockUtil.combineShapes(makeCuboidShape(0, 0, 0, 16, 1, 16), makeCuboidShape(1.5, 1, 1.5, 14.5, 2, 14.5), makeCuboidShape(3, 2, 3, 13, 3, 13), makeCuboidShape(4.5, 3, 4.5, 11.5, 4, 11.5), makeCuboidShape(6, 4, 6, 10, 8, 10), makeCuboidShape(5, 7, 6, 6, 9, 10), makeCuboidShape(11, 7, 10, 10, 9, 6), makeCuboidShape(6, 7, 5, 10, 9, 6), makeCuboidShape(10, 7, 11, 6, 9, 10), makeCuboidShape(9, 8, 12, 7, 10, 11), makeCuboidShape(12, 8, 9, 11, 10, 7), makeCuboidShape(4, 8, 7, 5, 10, 9), makeCuboidShape(7, 8, 4, 9, 10, 5));
     private static final VoxelShape NORTH = BlockUtil.combineShapes(makeCuboidShape(5, 11, 6.5, 11, 12, 9.5), makeCuboidShape(5, 14, 6.5, 11, 15, 9.5), makeCuboidShape(5, 12, 6.5, 6.5, 14, 9.5), makeCuboidShape(9.5, 12, 6.5, 11, 14, 9.5), makeCuboidShape(4, 11, 7.5, 12, 15, 8.5), makeCuboidShape(5, 10, 7.5, 11, 16, 8.5), makeCuboidShape(6.5, 12, 8.5, 9.5, 14, 9.5));
-    private static final VoxelShape EAST = BlockUtil.combineShapes(makeCuboidShape(9.5, 11, 5, 6.5, 12, 11), makeCuboidShape(9.5, 14, 5, 6.5, 15, 11), makeCuboidShape(9.5, 12, 5, 6.5, 14, 6.5), makeCuboidShape(9.5, 12, 9.5, 6.5, 14, 11), makeCuboidShape(8.5, 11, 4, 7.5, 15, 12), makeCuboidShape(8.5, 10, 5, 7.5, 16, 11), makeCuboidShape(9.5, 12, 6.5, 8.5, 14, 9.5));
+    private static final VoxelShape EAST = BlockUtil.combineShapes(makeCuboidShape(6.5, 11, 11, 9.5, 12, 5), makeCuboidShape(6.5, 14, 11, 9.5, 15, 5), makeCuboidShape(6.5, 12, 9.5, 9.5, 14, 11), makeCuboidShape(6.5, 12, 5, 9.5, 14, 6.5), makeCuboidShape(7.5, 11, 12, 8.5, 15, 4), makeCuboidShape(7.5, 10, 11, 8.5, 16, 5), makeCuboidShape(7.5, 12, 9.5, 6.5, 14, 6.5));
     private static final VoxelShape SOUTH = BlockUtil.combineShapes(makeCuboidShape(5, 10, 7.5, 11, 16, 8.5), makeCuboidShape(4, 11, 7.5, 12, 15, 8.5), makeCuboidShape(5, 11, 6.5, 11, 12, 9.5), makeCuboidShape(5, 14, 6.5, 11, 15, 9.5), makeCuboidShape(5, 12, 6.5, 6.5, 14, 9.5), makeCuboidShape(9.5, 12, 6.5, 11, 14, 9.5), makeCuboidShape(6.5, 12, 6.5, 9.5, 14, 7.5));
     private static final VoxelShape WEST = BlockUtil.combineShapes(makeCuboidShape(6.5, 11, 11, 9.5, 12, 5), makeCuboidShape(6.5, 14, 11, 9.5, 15, 5), makeCuboidShape(6.5, 12, 6.5, 9.5, 14, 5), makeCuboidShape(6.5, 12, 11, 9.5, 14, 9.5), makeCuboidShape(7.5, 11, 12, 8.5, 15, 4), makeCuboidShape(7.5, 10, 11, 8.5, 16, 5), makeCuboidShape(8.5, 12, 9.5, 9.5, 14, 6.5));
 
     public OcculusBlock() {
-        super(Properties.create(Material.ROCK).hardnessAndResistance(3, 5).harvestLevel(-1).harvestTool(ToolType.PICKAXE));
+        super(Properties.create(Material.ROCK).hardnessAndResistance(3, 5).harvestTool(ToolType.PICKAXE));
         setDefaultState(getDefaultState().with(FACING, Direction.NORTH));
     }
 
